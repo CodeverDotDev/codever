ec6eb87b5f5f3ff55e4a0d8afabe4c2acf6a0afb
Adrian Matei <ama@codingpedia.org>
2016-10-04 06:41:30 +0200

    add mocked GET method for bookmarks and categories

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var bookmarks = require('./routes/bookmarks');
+var categories = require('./routes/categories');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/bookmarks', bookmarks);
+app.use('/categories', categories);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
