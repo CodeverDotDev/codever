fe19b20a384394e92ae9d5abb49e6975ecc1c2a9
Adrian Matei <ama@codingpedia.org>
2016-10-05 06:52:09 +0200

    add category query parameter filter and also error handling when creating a bookmark

--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -4,20 +4,38 @@ var Bookmark = require('../models/bookmark');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  Bookmark.find({}, function(err, bookmarks){
-    if(err){
-      return res.send('Error');
-    }
-    res.send(bookmarks);
-  });
+  if(req.query.category){
+    Bookmark.find({category:req.query.category}, function(err, bookmarks){
+      if(err){
+        return res.send('Error');
+      }
+      res.send(bookmarks);
+    });
+  } else {
+    Bookmark.find({}, function(err, bookmarks){
+      if(err){
+        return res.send('Error');
+      }
+      res.send(bookmarks);
+    });
+  }
+
 });
 
 router.post('/', function(req, res, next){
   console.log(req.body);
   var bookmark = new Bookmark(req.body); //expect the model structure in the body directly
 
-  bookmark.save(); // save in the database
-  res.status(201).send('Bookmark created');
+  bookmark.save(function (err) {
+    if (err){
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(201).send('Bookmark created');
+    }
+    // saved!
+  });
+
 });
 
 module.exports = router;
