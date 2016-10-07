e99d98d5bbe80f9baffd06c3b9004a9fa761f4f9
Adrian Matei <ama@codingpedia.org>
2016-10-07 06:45:35 +0200

    CRUD operations on the bookmarks resource is ready - yeeey

--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -2,32 +2,34 @@ var express = require('express');
 var router = express.Router();
 var Bookmark = require('../models/bookmark');
 
-
 /* GET bookmark by id. */
 router.get('/:id', function(req, res, next) {
   Bookmark.findById(req.params.id, function(err, bookmark){
     if(err){
-      return res.send('Error');
+      console.log(err);
+      return res.status(500).send(err);
+    }
+    if(!bookmark){
+      return res.status(404).send("Bookmark not found");
     }
     res.send(bookmark);
   });
 
 });
 
-
 /* GET bookmarks listing. */
 router.get('/', function(req, res, next) {
   if(req.query.category){
     Bookmark.find({category:req.query.category}, function(err, bookmarks){
       if(err){
-        return res.send('Error');
+        return res.status(500).send(err);
       }
       res.send(bookmarks);
     });
   } else {
     Bookmark.find({}, function(err, bookmarks){
       if(err){
-        return res.send('Error');
+        return res.status(500).send(err);
       }
       res.send(bookmarks);
     });
@@ -35,6 +37,9 @@ router.get('/', function(req, res, next) {
 
 });
 
+/**
+ * CREATE bookmark
+ */
 router.post('/', function(req, res, next){
   console.log(req.body);
   var bookmark = new Bookmark(req.body); //expect the model structure in the body directly
@@ -48,6 +53,38 @@ router.post('/', function(req, res, next){
       res.status(201).send('Bookmark created');
     }
     // saved!
+  });
+
+});
+
+/**
+ * full UPDATE via PUT - that is the whole document is required and will be updated
+ */
+router.put('/:id', function(req, res, next) {
+  Bookmark.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, bookmark){
+    if(err){
+      return res.status(500).send(err);
+    }
+    if(!bookmark){
+      return res.status(404).send('Bookmark not found');
+    }
+    res.status(200).send(bookmark);
+  });
+
+});
+
+/**
+ * DELETE
+ */
+router.delete('/:id', function(req, res, next) {
+  Bookmark.findByIdAndRemove(req.params.id, function(err, bookmark){
+    if(err){
+      return res.status(500).send(err);
+    }
+    if(!bookmark){
+      return res.status(404).send('Bookmark not found');
+    }
+    res.status(204).send('Bookmark successfully deleted');
   });
 
 });
