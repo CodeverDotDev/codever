22b8af4a4b70a09a79be58b28a7e3877f8c39a6f
Adrian Matei <ama@codingpedia.org>
2016-10-10 09:28:51 +0200

    separate bookmark details view

--- src/app/bookmark/bookmark.service.ts
+++ src/app/bookmark/bookmark.service.ts
@@ -5,7 +5,14 @@ import { BOOKMARKS } from './mock-bookmarks';
 
 @Injectable()
 export class BookmarkService {
+
   getBookmarks(): Promise<Bookmark[]> {
     return Promise.resolve(BOOKMARKS);
   }
+
+  getBookmark(id: string): Promise<Bookmark> {
+    return this.getBookmarks()
+      .then(bookmarks => bookmarks.find(bookmark => bookmark.id === id));
+  }
+
 }
--- src/app/bookmark/mock-bookmarks.ts
+++ src/app/bookmark/mock-bookmarks.ts
@@ -1,6 +1,6 @@
 import { Bookmark } from './bookmark';
 export const BOOKMARKS: Bookmark[] = [
-  {"name": "java bookmark", "location":"java location", "description":"some description", "category":"java", "tags":["tag1", "tag2"]},
-  {"name": "git bookmark", "location":"git location", "description":"some description", "category":"git", "tags":["tag1", "tag2"]},
-  {"name": "js bookmark", "location":"js location", "description":"some description", "category":"java", "tags":["tag1", "tag2"]}
+  {'id':'34234f3', 'name': 'java bookmark', 'location':'java location', 'description':'some description', 'category':'java', 'tags':['tag1', 'tag2']},
+  {'id':'55234f3', 'name': 'git bookmark', 'location':'git location', 'description':'some description', 'category':'git', 'tags':['tag1', 'tag2']},
+  {'id':'88234f3', 'name': 'js bookmark', 'location':'js location', 'description':'some description', 'category':'java', 'tags':['tag1', 'tag2']}
 ];
