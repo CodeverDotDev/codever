422adb72fd5373abfc79b1c4dc045e67a2d0ed8f
ama <ama@codepedia.org>
2016-10-12 17:45:46 +0200

    add codingpedia url

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { ApiService } from './shared';
+
+import '../style/app.scss';
+
+@Component({
+  selector: 'my-app', // <my-app></my-app>
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  url = 'https://github.com/Codingpedia/bookmarks';
+
+  constructor(private api: ApiService) {
+    // Do something with api
+  }
+}
