13c6ea5a2eba4aa37c905d9052847bd26f24b3ec
Adrian Matei <ama@codingpedia.org>
2016-10-12 07:34:32 +0200

    implement GET bookmarks from backend - not being shown yet...

--- src/app/bookmark/bookmark.service.ts
+++ src/app/bookmark/bookmark.service.ts
@@ -2,17 +2,38 @@ import {Injectable} from '@angular/core';
 import {Bookmark} from './bookmark';
 
 import { BOOKMARKS } from './mock-bookmarks';
+import {Http} from "@angular/http";
+
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class BookmarkService {
 
+  private bookmarksUrl = 'http://localhost:3000/bookmarks/';  // URL to web api
+
+  constructor(private http: Http) { }
+
+  getBookmarks(): Promise<Bookmark[]> {
+    return this.http.get(this.bookmarksUrl)
+      .toPromise()
+      .then(response => response.json().data as Bookmark[])
+      .catch(this.handleError);
+  }
+
+
+  /*
   getBookmarks(): Promise<Bookmark[]> {
     return Promise.resolve(BOOKMARKS);
-  }
+  } */
 
   getBookmark(id: string): Promise<Bookmark> {
     return this.getBookmarks()
-      .then(bookmarks => bookmarks.find(bookmark => bookmark.id === id));
+      .then(bookmarks => bookmarks.find(bookmark => bookmark._id === id));
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 
 }
