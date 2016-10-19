058377ce770b3670422dafebba3f0de4c2c8a9be
Adrian Matei <ama@codingpedia.org>
2016-10-19 07:22:34 +0200

    start working on the New Bookmark component via template form

--- src/app/bookmark/bookmark.ts
+++ src/app/bookmark/bookmark.ts
@@ -6,9 +6,8 @@ export class Bookmark {
     category: string;
     tags: string[];
 
-    constructor (_id: string, name: string, location: string, category: string, tags: string[], description?: string){
+    constructor (name: string, location: string, category: string, tags: string[], description?: string){
         this.name = name;
-        this._id = _id;
         this.location = location;
         this.description = description;
         this.category = category;
---
+++ src/app/bookmark/form/bookmark-form.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {Bookmark} from "../bookmark";
+
+@Component({
+  selector: 'bookmark-form',
+  templateUrl: 'bookmark-form.component.html'
+})
+export class BookmarkFormComponent {
+
+  model = new Bookmark('test', 'usually a url', 'java', ['jvm', 'performance'], 'some description to search through later');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
+}
