2dfc764ef2d24aba8552c15e90be8360c8a1a492
ama <ama@codepedia.org>
2016-10-20 18:35:11 +0200

    works so far, except the pattern validation for tags...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, ApplicationRef } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -23,6 +23,7 @@ import {BookmarkFormComponent} from "./bookmark/form/bookmark-form.component";
     BrowserModule,
     HttpModule,
     FormsModule,
+    ReactiveFormsModule,
     routing
   ],
   declarations: [
