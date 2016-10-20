f4795dcb89a9bf1f51e312d68421753779270e38
ama <adrian.matei@bkw.ch>
2016-10-20 18:35:11 +0200

    works so far, except the pattern validation for tags...

--- src/app/bookmark/form/bookmark-form.component.ts
+++ src/app/bookmark/form/bookmark-form.component.ts
@@ -1,24 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Bookmark} from "../bookmark";
 import {BookmarkService} from "../bookmark.service";
 import {Location} from "@angular/common";
+import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'bookmark-form',
   templateUrl: 'bookmark-form.component.html',
   styleUrls: ['./bookmark-form.component.scss']
 })
-export class BookmarkFormComponent {
+export class BookmarkFormComponent implements OnInit {
 
   model = new Bookmark('', '', '', [], '');
 
   submitted = false;
   active=true;
 
+  newBookmarkForm: FormGroup;
+
   constructor(
     private bookmarkService: BookmarkService,
-    private location: Location
+    private location: Location,
+    private formBuilder: FormBuilder
   ) {}
+
+  ngOnInit(): void {
+    this.newBookmarkForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      location: ['', Validators.required],
+      category: ['', Validators.required],
+      tags:['', Validators.pattern("[^,]+")],
+      description:''
+    });
+  }
 
   onSubmit() {
     this.submitted = true;
@@ -37,5 +51,10 @@ export class BookmarkFormComponent {
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(model: Bookmark, isValid: boolean) {
+    this.submitted = true;
+    console.log(model, isValid);
   }
 }
