78ae78e8b60b636eaa27dac616d88932dab4b38f
Adrian Matei <ama@codepedia.org>
2016-10-31 10:17:28 +0100

    introduce async bookmark list component to avoid code duplication - (component is currently used by allBookmarks and the search component)

--- src/app/bookmark/bookmarks.component.ts
+++ src/app/bookmark/bookmarks.component.ts
@@ -11,13 +11,13 @@ import {Observable} from "rxjs";
 })
 export class BookmarksComponent implements  OnInit{
 
-  bookmarks: Observable<Bookmark[]>;
+  allBookmarks: Observable<Bookmark[]>;
   selectedBookmark: Bookmark;
 
   constructor( private router: Router, private bookmarkService: BookmarkService) { }
 
   getBookmarks(): void {
-    this.bookmarks = this.bookmarkService.getBookmarks();
+    this.allBookmarks = this.bookmarkService.getBookmarks();
   }
 
   ngOnInit(): void {
@@ -26,40 +26,6 @@ export class BookmarksComponent implements  OnInit{
 
   onSelect(bookmark: Bookmark): void {
     this.selectedBookmark = bookmark;
-  }
-
-  /**
-   *
-   * @param bookmark
-   */
-  gotoDetail(bookmark: Bookmark): void {
-    let link = ['/bookmarks', bookmark._id];
-    this.router.navigate(link);
-  }
-
-
-  delete(bookmark: Bookmark): void {
-    this.bookmarkService
-        .delete(bookmark._id)
-        .subscribe(
-          data => {
-            // refresh the list
-            this.bookmarks.map(h => h.filter(x => x !== bookmark));
-            //this.getBookmarks();
-            return true;
-          },
-          error => {
-            console.error("Error deleting bookmark!");
-            return Observable.throw(error);
-          });
-    /*
-
-        .map(this.bookmarks.map(h => h !== bookmark);
-        .then(() => {
-          this.bookmarks = this.bookmarks.map(h => h !== bookmark);
-          if (this.selectedBookmark === bookmark) { this.selectedBookmark = null; }
-        });
-        */
   }
 
 }
