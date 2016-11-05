26eb306e7b84729b33dbfb1795f12758aa4e54a6
Adrian Matei <ama@codingpedia.org>
2016-11-05 07:09:33 +0100

    create navigation component

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-navigation',
+  templateUrl: './navigation.component.html',
+})
+export class NavigationComponent {
+
+}
