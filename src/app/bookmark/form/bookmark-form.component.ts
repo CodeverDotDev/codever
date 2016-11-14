bbb16309fdd6d5ebf0ff30d447e018c9b4994f19
Adrian Matei <ama@codingpedia.org>
2016-11-14 06:29:49 +0100

    cleanup bookmark-form class

--- src/app/bookmark/form/bookmark-form.component.ts
+++ src/app/bookmark/form/bookmark-form.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {Bookmark} from "../../model/bookmark";
-import {BookmarkService} from "../bookmark.service";
 import {Location} from "@angular/common";
 import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 import {BookmarkStore} from "../store/BookmarkStore";
@@ -20,7 +19,6 @@ export class BookmarkFormComponent implements OnInit {
   bookmarkForm: FormGroup;
 
   constructor(
-    private bookmarkService: BookmarkService,
     private bookmarkStore: BookmarkStore,
     private location: Location,
     private formBuilder: FormBuilder
@@ -36,41 +34,13 @@ export class BookmarkFormComponent implements OnInit {
     });
   }
 
-  onSubmit() {
-    this.submitted = true;
-    this.bookmarkService.create(this.model)
-      .then(() => this.goBack());//TODO - go to bookmark details after creation
-  }
-
-  newBookmark() {
-    this.model = new Bookmark('', '', '', [], '');
-    this.active = false;
-    setTimeout(() => this.active = true, 0);
-  }
-
-  // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.model); }
-
   goBack(): void {
     this.location.back();
-  }
-
-  save(model: Bookmark, isValid: boolean) {
-    this.submitted = true;
-    model.tags = model.tagsLine.split(",");
-
-    var newBookmark = new Bookmark(model.name, model.location, model.category,model.tagsLine.split(","), model.description);
-
-
-    console.log(model, isValid);
-    this.bookmarkService.create(newBookmark)
-      .then(() => this.goBack());
   }
 
   saveBookmark(model: Bookmark, isValid: boolean) {
     model.tags = model.tagsLine.split(",");
     var newBookmark = new Bookmark(model.name, model.location, model.category,model.tagsLine.split(","), model.description);
-
 
     let obs = this.bookmarkStore.addBookmark(newBookmark);
 
