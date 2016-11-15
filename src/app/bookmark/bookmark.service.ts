f4fc294c75062e67b1b9d65a5f5022650632a086
Adrian Matei <ama@codingpedia.org>
2016-11-15 06:09:48 +0100

    remove unused methods

--- src/app/bookmark/bookmark.service.ts
+++ src/app/bookmark/bookmark.service.ts
@@ -14,52 +14,9 @@ export class BookmarkService {
 
   constructor(private http: Http) { }
 
-  create(bookmark:Bookmark): Promise<Bookmark> {
-    return this.http
-      .post(this.bookmarksUrl, JSON.stringify(bookmark), {headers: this.headers})
-      .toPromise()
-      .then(res => res.json().data)
-      .catch(this.handleError);
-  }
-
-  /**
-   * TODO use .map for DTO
-   * @returns {Observable<R>|Promise<R>|Q.Promise<*>|Promise<*|T>|Promise<*>|any}
-   */
-  getBookmarks(): Observable<Bookmark[]> {
-    console.log('******** BookmarkService.getBookmarks was called *************')
-    return this.http.get(this.bookmarksUrl)
-    // ...and calling .json() on the response to return data
-      .map((res:Response) => res.json())
-      //...errors if any
-      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
-  }
-
   getAllBookmarks(): Observable<Response> {
     console.log('******** BookmarkService.getAllBookmarks was called *************')
     return this.http.get(this.bookmarksUrl);
-  }
-
-  getBookmarksObservable(): Observable<Bookmark[]> {
-    return this.http.get(this.bookmarksUrl)
-    // ...and calling .json() on the response to return data
-      .map((res:Response) => res.json())
-      //...errors if any
-      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
-  }
-
-  getBookmark(id: string): Promise<Bookmark> {
-    return this.getBookmarks().toPromise()
-      .then(bookmarks => bookmarks.find(bookmark => bookmark._id === id));
-  }
-
-  update(bookmark:Bookmark): Promise<Bookmark> {
-    const url = `${this.bookmarksUrl}/${bookmark._id}`;
-    return this.http
-    .put(url, JSON.stringify(bookmark), {headers: this.headers})
-    .toPromise()
-    .then(() => bookmark)
-    .catch(this.handleError);
   }
 
   updateBookmark(bookmark:Bookmark): Observable<any> {
@@ -74,18 +31,6 @@ export class BookmarkService {
     return this.http.delete(url, {headers: this.headers}).share();
         //.map((res:Response) => res.json()) // ...and calling .json() on the response to return data
         //.catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
-  }
-
-  deleteBookmark(id: string): Observable<Bookmark[]> {
-    const url = `${this.bookmarksUrl}/${id}`;
-    return this.http.delete(url, {headers: this.headers})
-        .map((res:Response) => res.json()) // ...and calling .json() on the response to return data
-        .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
-  }
-
-  private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
-    return Promise.reject(error.message || error);
   }
 
   saveBookmark(bookmark: Bookmark): Observable<any> {
