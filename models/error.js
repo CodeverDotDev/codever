be9a30572401f487e14d720c05e868a2cc1f41c6
Adrian Matei <ama@codepedia.org>
2016-11-21 19:26:08 +0100

    homogenize error handling in backend - deliver same error message format

--- models/error.js
+++ models/error.js
@@ -1,6 +1,6 @@
-var Error = function (title, messages) {
+var MyError = function (title, messages) {
   this.title = title;
   this.messages = messages;
 };
 
-module.exports = Error;
+module.exports = MyError;
