d908398316bf639a57768f2d3213b86bbebb5985
Adrian Matei <ama@codepedia.org>
2016-12-13 07:10:36 +0100

    add Access-Control-Expose-Headers - see  https://www.html5rocks.com/en/tutorials/cors/

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, PATCH, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Location');
+  res.setHeader('Access-Control-Expose-Headers', 'Content-Type, Authorization, Location');
   next();
 });
 
