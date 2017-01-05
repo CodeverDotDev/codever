7d1ee680be76a7ad79652594381560fcfd4a9d2f
Adrian Matei <ama@codepedia.org>
2017-01-05 07:16:40 +0100

    update filter service to accept multiple terms separated by "+"

--- src/app/filter.service.ts
+++ src/app/filter.service.ts
@@ -6,40 +6,34 @@ import {List} from "immutable";
 @Injectable()
 export class BookmarkFilterService {
 
-  filterBookmarksBySearchTerm(term:string, bookmarks:List<Bookmark>): Bookmark[] {
-    let filteredBookmarks: Array<Bookmark> = new Array();
-    bookmarks.forEach(bookmark => {
-      let hit:boolean =false;
-      if(bookmark.name.toLowerCase().indexOf(term.toLowerCase()) !== -1
-        || bookmark.description.toLowerCase().indexOf(term.toLowerCase()) !== -1
-        || bookmark.category.toLowerCase().indexOf(term.toLowerCase()) !== -1
-        || bookmark.tags.indexOf(term.toLowerCase()) !== -1
-      ){
-        filteredBookmarks.push(bookmark);
-        hit = true;
-      }
+  filterBookmarksBySearchTerm(query:string, bookmarks:List<Bookmark>): Bookmark[] {
+    var terms = query.split("+");
+    let filteredBookmarks = bookmarks;
+    terms.forEach(term => {
+      filteredBookmarks = filteredBookmarks.filter(x => this.bookmarkContainsTerm(x, term.trim())).toList();
+    });
 
-      //if not hit look throught the tags also
-      let hitInTags: boolean = false;
-      if(!hit){
-        bookmark.tags.forEach(tag => {
-          if(tag.indexOf(term.toLowerCase()) !== -1){
-            hitInTags = true;
-          }
-        });
-      }
+    return filteredBookmarks.toArray();
+  }
 
-      if(!hit && hitInTags){
-        filteredBookmarks.push(bookmark);
+
+  private bookmarkContainsTerm(bookmark: Bookmark, term: string):boolean {
+    let hit:boolean =false;
+    if(bookmark.name.toLowerCase().indexOf(term.toLowerCase()) !== -1
+      || bookmark.description.toLowerCase().indexOf(term.toLowerCase()) !== -1
+      || bookmark.category.toLowerCase().indexOf(term.toLowerCase()) !== -1
+      || bookmark.tags.indexOf(term.toLowerCase()) !== -1
+    ){
+      return true;
+    }
+
+    //if not hit look throught the tags also
+    bookmark.tags.forEach(tag => {
+      if(tag.indexOf(term.toLowerCase()) !== -1){
+        return true;
       }
     });
 
-    let  filteredBookmarksArray: Array<Array<Bookmark>> = new Array();
-    filteredBookmarksArray.push(filteredBookmarks);
-
-    console.log('I have been here, size ' + filteredBookmarks.length);
-
-    return filteredBookmarks;
+    return false;
   }
-
 }
