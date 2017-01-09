36ba03e044fcee77c08852da79933333933f96fa
Adrian Matei <ama@codepedia.org>
2017-01-09 17:12:25 +0100

    add bookmarks grouped by tags in the home page - yees

--- src/app/bookmark/bookmarks.component.ts
+++ src/app/bookmark/bookmarks.component.ts
@@ -3,6 +3,7 @@ import {Bookmark} from "../model/bookmark";
 import {Observable} from "rxjs";
 import {BookmarkStore} from "./store/BookmarkStore";
 import {List} from "immutable";
+import {Tag} from "../model/tags";
 
 @Component({
   selector: 'my-bookmarks',
@@ -12,6 +13,7 @@ import {List} from "immutable";
 export class BookmarksComponent implements  OnInit{
 
   publicBookmarks: Observable<List<Bookmark>>;
+  tags: Tag[] = [];
 
   constructor(private bookmarkStore: BookmarkStore) { }
 
@@ -21,6 +23,55 @@ export class BookmarksComponent implements  OnInit{
 
   ngOnInit(): void {
     this.getBookmarks();
+
+    this.publicBookmarks.subscribe(
+      bookmarks => {
+        let allTags = new Set();
+
+        bookmarks.forEach(bookmark => {
+          //allTags.merge(allTags, OrderedSet.fromKeys(bookmark.tags));
+          bookmark.tags.forEach(tag => {
+            allTags = allTags.add(tag.trim().toLowerCase());
+          });
+        });
+
+
+        console.log("all TAgs array");
+        console.log(Array.from(allTags).sort());
+
+        Array.from(allTags).sort().forEach(tag => {
+          let tagBookmarks = [];
+          bookmarks.forEach(bookmark => {
+            bookmark.tags.forEach(bookmarkTag => {
+              if(bookmarkTag.trim().toLowerCase() === tag){
+                tagBookmarks.push(bookmark);
+              }
+            });
+          });
+          this.tags.push(new Tag(tag, tagBookmarks));
+        });
+      },
+      err => {
+        console.log("Error filtering bookmakrs");
+      }
+    );
+
+
+
+    /**/
+    /*
+    //now iterated through all tags and build the tags[] list
+    let map = allTags.map(x => {
+      let bookmarksWithTag = [];
+      bookmarks.forEach(bookmark => {
+        if(bookmark.tags.indexOf(x.toLowerCase()) !== -1){
+          bookmarksWithTag.push(bookmark);
+        }
+      });
+
+      return new Tag(x, bookmarksWithTag);
+    });
+    */
   }
 
 }
