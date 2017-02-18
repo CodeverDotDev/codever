3d68659a384d141b1b33e02461ecd93164b2d926
Adrian Matei <ama@codingpedia.org>
2017-02-18 05:55:58 +0100

    make the search look through the location too; add method comments to filter service; remove unused searchFilter methods from store classes

--- src/app/personal/store/UserBookmarkStore.ts
+++ src/app/personal/store/UserBookmarkStore.ts
@@ -136,45 +136,5 @@ export class UserBookmarkStore {
     return bookmarks.get(index);
   }
 
-  filterBookmarksBySearchTerm(term:string): Bookmark[] {
-    let bookmarks: List<Bookmark> = this._bookmarks.getValue();
-    let filteredBookmarks: Array<Bookmark> = new Array();
-    bookmarks.forEach(bookmark => {
-      let hit:boolean =false;
-      if(bookmark.name.toLowerCase().indexOf(term.toLowerCase()) !== -1
-        || bookmark.description.toLowerCase().indexOf(term.toLowerCase()) !== -1
-        || bookmark.category.toLowerCase().indexOf(term.toLowerCase()) !== -1
-        || bookmark.tags.indexOf(term.toLowerCase()) !== -1
-      ){
-        filteredBookmarks.push(bookmark);
-        hit = true;
-      }
-
-      //if not hit look throught the tags also
-      let hitInTags: boolean = false;
-      if(!hit){
-        bookmark.tags.forEach(tag => {
-          if(tag.indexOf(term.toLowerCase()) !== -1){
-            hitInTags = true;
-          }
-        });
-      }
-
-      if(!hit && hitInTags){
-        filteredBookmarks.push(bookmark);
-      }
-    });
-
-    let  filteredBookmarksArray: Array<Array<Bookmark>> = new Array();
-    filteredBookmarksArray.push(filteredBookmarks);
-
-    console.log('I have been here, size ' + filteredBookmarks.length);
-
-    return filteredBookmarks;
-  }
-
-  getBookmarksValue():List<Bookmark> {
-    return this._bookmarks.getValue();
-  }
 }
 
