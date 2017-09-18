e29f2cbd1c3da761c947ddb50babe8c23084a462
Adrian Matei <ama@codepedia.org>
2017-09-18 07:29:40 +0200

    remove ngZone - not needed anymore

--- src/app/personal/async-list/async-personal-bookmark-list.component.ts
+++ src/app/personal/async-list/async-personal-bookmark-list.component.ts
@@ -1,8 +1,8 @@
-import {Component, Input, NgZone} from '@angular/core';
-import {Router, ActivatedRoute} from '@angular/router';
-import {Observable} from 'rxjs/Observable';
-import {Bookmark} from '../../core/model/bookmark';
-import {PersonalBookmarksStore} from '../store/PersonalBookmarksStore';
+import {Component, Input} from "@angular/core";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Observable} from "rxjs/Observable";
+import {Bookmark} from "../../core/model/bookmark";
+import {PersonalBookmarksStore} from "../store/PersonalBookmarksStore";
 
 @Component({
   selector: 'my-async-personal-bookmark-list',
@@ -15,7 +15,6 @@ export class AsyncUserBookmarksListComponent{
   bookmarks: Observable<Bookmark[]>;
 
   constructor(
-    private zone: NgZone, // TODO without explicitly running the zone functionality the view does not get updated, though model and everything gets updated
     private route: ActivatedRoute,
     private router: Router,
     private userBookmarkStore: PersonalBookmarksStore) {}
@@ -30,14 +29,7 @@ export class AsyncUserBookmarksListComponent{
   }
 
   deleteBookmark(bookmark: Bookmark): void {
-
     const obs = this.userBookmarkStore.deleteBookmark(bookmark);
-    obs.subscribe(
-      res => {
-        this.zone.run(() => {
-          console.log('ZONE RUN bookmark deleted');
-        });
-      });
   }
 
 }
