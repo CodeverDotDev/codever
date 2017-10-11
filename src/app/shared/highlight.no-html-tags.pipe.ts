9a75f8610c4ed25da5cf9baf1d1545fae42f5242
Adrian Matei <ama@codepedia.org>
2017-10-11 08:16:03 +0200

    remove commented code

--- src/app/shared/highlight.no-html-tags.pipe.ts
+++ src/app/shared/highlight.no-html-tags.pipe.ts
@@ -1,20 +1,16 @@
-
-
-import {Pipe} from "@angular/core";
-import {PipeTransform} from "@angular/core";
+import {Pipe} from '@angular/core';
+import {PipeTransform} from '@angular/core';
 
 @Pipe({ name: 'highlightHtml' })
 export class HighLightHtmlPipe implements PipeTransform {
 
   transform(text: string, search): string {
-    var pattern = search.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    let pattern = search.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
     pattern = pattern.split(' ').filter((t) => {
       return t.length > 0;
     }).join('|');
-    //pattern = '(' + pattern + ')' + '(?![^<]*>|[<>]*</)';
-    //pattern = '(' + pattern + ')' + '(?![^<]*>|[<>]*<\/)';
     pattern = '(' + pattern + ')' + '(?![^<]*>)';
-    var regex = new RegExp(pattern, 'gi');
+    const regex = new RegExp(pattern, 'gi');
 
     return search ? text.replace(regex, (match) => `<span class="highlight">${match}</span>`) : text;
   }
