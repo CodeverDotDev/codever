386f02a1a8c9bf63b57ef693613b2cda65020979
Adrian Matei <ama@codepedia.org>
2018-06-16 22:00:28 +0200

    first trial, typeerror

--- src/app/personal/create/create-new-personal-bookmark.component.ts
+++ src/app/personal/create/create-new-personal-bookmark.component.ts
@@ -1,14 +1,16 @@
 import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Bookmark} from '../../core/model/bookmark';
-import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {PersonalBookmarksStore} from '../../core/store/PersonalBookmarksStore';
 import {MarkdownService} from '../markdown.service';
 import {KeycloakService} from 'keycloak-angular';
 import {BookmarkStore} from '../../public/bookmark/store/BookmarkStore';
 import {PublicBookmarksService} from '../../public/bookmark/public-bookmarks.service';
 import {COMMA, ENTER, SPACE} from '@angular/cdk/keycodes';
-import {MatChipInputEvent} from '@angular/material';
+import {MatAutocompleteSelectedEvent, MatChipInputEvent} from '@angular/material';
+import {Observable} from 'rxjs/index';
+import {map, startWith} from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-new-personal-bookmark-form',
@@ -32,6 +34,20 @@ export class CreateNewPersonalBookmarkComponent implements OnInit {
   // Enter, comma, space
   separatorKeysCodes = [ENTER, COMMA, SPACE];
 
+  allTags = [
+    'Apple',
+    'Lemon',
+    'Lime',
+    'Orange',
+    'Strawberry'
+  ];
+
+  tagCtrl = new FormControl();
+
+  filteredTags: Observable<any[]>;
+
+  @ViewChild('tagInput') tagInput: ElementRef;
+
   constructor(
     private personalBookmarksStore: PersonalBookmarksStore,
     private formBuilder: FormBuilder,
@@ -40,10 +56,31 @@ export class CreateNewPersonalBookmarkComponent implements OnInit {
     private markdownServce: MarkdownService,
     private publicBookmarkStore: BookmarkStore
   ) {
-    keycloakService.loadUserProfile().then( keycloakProfile => {
-    this.userId = keycloakProfile.id;
-  });
 
+    keycloakService.loadUserProfile().then( keycloakProfile => {
+      this.userId = keycloakProfile.id;
+    });
+
+    this.filteredTags = this.tagCtrl.valueChanges.pipe(
+      startWith(null),
+      map((tag: string | null) => tag ? this.filter(tag) : this.allTags.slice())
+/*      startWith(''),
+      map(val => this.filter(val))*/
+    );
+  }
+
+/*  filter(val: string): string[] {
+    return this.allTags.filter(option => option.toLowerCase().indexOf(val.toLowerCase()) === 0);
+  }*/
+  filter(name: string) {
+    return this.allTags.filter(tag => tag.toLowerCase().indexOf(name.toLowerCase()) === 0);
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    const tags = this.bookmarkForm.get('tags') as FormArray;
+    tags.push(this.formBuilder.control(event.option.viewValue));
+    this.tagInput.nativeElement.value = '';
+    this.tagCtrl.setValue(null);
   }
 
   ngOnInit(): void {
@@ -156,6 +193,8 @@ export class CreateNewPersonalBookmarkComponent implements OnInit {
     if (input) {
       input.value = '';
     }
+
+    this.tagCtrl.setValue(null);
   }
 
   remove(index: number): void {
@@ -168,8 +207,6 @@ export class CreateNewPersonalBookmarkComponent implements OnInit {
 
   getTagsErrorMessage() {
     if (this.bookmarkForm.get('tags').hasError('required')) {
-      console.log(this.bookmarkForm.get('tags').errors, 'erori :');
-      //consothis.bookmarkForm.get('tags').status
       return 'You must enter a value';
     }
 /*    return this.bookmarkForm.get('tags').hasError('required') ? 'You must enter a value' :
