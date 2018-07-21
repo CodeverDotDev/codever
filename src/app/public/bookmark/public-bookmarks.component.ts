d45ef66cea58ffe6ea1d2019a3388d931eb3efcf
Adrian Matei <ama@codepedia.org>
2018-07-21 07:06:26 +0200

    display tags from constant list; To do - move tags to secondary page and take it from backend...

--- src/app/public/bookmark/public-bookmarks.component.ts
+++ src/app/public/bookmark/public-bookmarks.component.ts
@@ -2,10 +2,10 @@ import {Component, NgZone, OnInit, ViewChild} from '@angular/core';
 import {Observable} from 'rxjs';
 import {List} from 'immutable';
 import {Bookmark} from '../../core/model/bookmark';
-import {Tag} from '../../core/model/tags';
 import {ActivatedRoute} from '@angular/router';
 import {BookmarkSearchComponent} from '../../shared/search/bookmark-search.component';
 import {PublicBookmarksStore} from './store/public-bookmarks.store';
+import {allTags} from '../../core/model/all-tags.const.en';
 
 
 @Component({
@@ -16,7 +16,7 @@ import {PublicBookmarksStore} from './store/public-bookmarks.store';
 export class PublicBookmarksComponent implements OnInit {
 
   publicBookmarks: Observable<List<Bookmark>>;
-  tags: Tag[] = [];
+  tags: string[] = allTags;
   query = '';
 
   @ViewChild(BookmarkSearchComponent)
@@ -26,16 +26,6 @@ export class PublicBookmarksComponent implements OnInit {
               private route: ActivatedRoute,
               private zone: NgZone
               ) { }
-
-  getBookmarks(): void {
-    this.publicBookmarks = this.bookmarkStore.getBookmarks();
-    this.publicBookmarks.subscribe(
-      res => {
-        this.zone.run(() => {
-          console.log('ZONE RUN for initial load of bookmarks'); // need to investigate this, or merge it with the async list stuff....
-        });
-      });
-  }
 
   ngOnInit(): void {
 
@@ -48,37 +38,18 @@ export class PublicBookmarksComponent implements OnInit {
     }
 
     this.getBookmarks();
-
-    this.publicBookmarks.subscribe(
-      bookmarks => {
-        let allTags = new Set();
-
-        bookmarks.forEach(bookmark => {
-          // allTags.merge(allTags, OrderedSet.fromKeys(bookmark.tags));
-          bookmark.tags.forEach(tag => {
-            allTags = allTags.add(tag.trim().toLowerCase());
-          });
-        });
-
-
-        Array.from(allTags).sort().forEach(tag => {
-          const tagBookmarks = [];
-          bookmarks.forEach(bookmark => {
-            bookmark.tags.forEach(bookmarkTag => {
-              if (bookmarkTag.trim().toLowerCase() === tag){
-                tagBookmarks.push(bookmark);
-              }
-            });
-          });
-
-          this.tags.push(new Tag(tag.toString(), tagBookmarks));
-        });
-      },
-      err => {
-        console.log('Error filtering bookmakrs');
-      }
-    );
   }
+
+  getBookmarks(): void {
+    this.publicBookmarks = this.bookmarkStore.getBookmarks();
+    this.publicBookmarks.subscribe(
+      res => {
+        this.zone.run(() => {
+          console.log('ZONE RUN for initial load of bookmarks'); // need to investigate this, or merge it with the async list stuff....
+        });
+      });
+  }
+
 
   onTagClick(tag: string) {
     this.searchComponent.setQueryFromParentComponent('[' + tag + ']');
