2d66727504841e7735c71a441dd16176fc9813de
Adrian Matei <ama@cl151905.bkw-fmb.ch>
2018-07-25 17:40:14 +0200

    test against localhost api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  APP_HOME_URL: 'http://localhost:4200',
-  API_URL: 'https://www.codingmarks.org/api',
-  HOST: 'http://localhost:4200/',
+    production: false,
+    APP_HOME_URL: 'http://localhost:4200',
+    API_URL: 'http://localhost:3000/api',
+    HOST: 'http://localhost:4200/',
     keycloak:  {
         'realm': 'codingpedia',
-        'url': 'https://www.codingmarks.org/auth',
-        'clientId': 'codingmarks'
+        'url': 'http://localhost:8380/auth',
+        'clientId': 'bookmarks'
     }
 };
