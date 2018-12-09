f996389fa8204d21db1087fbacd0c698e04d0417
Adrian Matei <ama@codingpedia.org>
2018-12-09 07:06:29 +0100

    refactoring - get rid of some magic numbers
    introduced https status module to avoid writing http status by hand

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('API Backend supporting Codingpedia Bookmarks');
+  res.send('API Backend supporting Codingmarks.org');
 });
 
 module.exports = router;
