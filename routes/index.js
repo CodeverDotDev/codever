1c9a1a0fd2d90ba4c3da66f1707a200ab7c080f0
Adrian Matei <ama@codingpedia.org>
2018-12-15 20:57:00 +0100

    add default api path get to swagger docs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('API Backend supporting Codingmarks.org');
+  res.send('API Backend supporting Codingmarks.org - <a href="/api/docs">Swagger Docs</a>');
 });
 
 module.exports = router;
