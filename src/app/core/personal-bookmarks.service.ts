7d2cc776ba68732d8416fd3892b624860721d1fb
Adrian Matei <ama@codingpedia.org>
2018-12-26 12:35:50 +0100

    change api url from "bookmarks" to "codingmarks"

--- src/app/core/personal-bookmarks.service.ts
+++ src/app/core/personal-bookmarks.service.ts
@@ -18,29 +18,28 @@ export class PersonalBookmarksService {
   private headers = new HttpHeaders({'Content-Type': 'application/json'});
 
   constructor(private httpClient: HttpClient) {
-    // this.baseUrl = process.env.API_URL + '/users/';
     this.baseUrl = environment.API_URL + '/private/users/';
   }
 
   getAllBookmarks(userId: String): Observable<Bookmark[]> {
-    return this.httpClient.get<Bookmark[]>(this.baseUrl + userId + '/bookmarks').pipe(shareReplay());
+    return this.httpClient.get<Bookmark[]>(this.baseUrl + userId + '/codingmarks').pipe(shareReplay());
   }
 
   updateBookmark(bookmark: Bookmark): Observable<any> {
     return this.httpClient
-      .put(this.baseUrl + bookmark.userId + '/bookmarks/' + bookmark._id, JSON.stringify(bookmark), {headers: this.headers})
+      .put(this.baseUrl + bookmark.userId + '/codingmarks/' + bookmark._id, JSON.stringify(bookmark), {headers: this.headers})
       .pipe(shareReplay());
   }
 
   delete(bookmark: Bookmark): Observable<any> {
     return this.httpClient
-      .delete(this.baseUrl + bookmark.userId + '/bookmarks/' + bookmark._id, {headers: this.headers})
+      .delete(this.baseUrl + bookmark.userId + '/codingmarks/' + bookmark._id, {headers: this.headers})
       .pipe(shareReplay());
   }
 
   saveBookmark(userId: string, bookmark: Bookmark): Observable<any> {
     return this.httpClient
-      .post(this.baseUrl + userId + '/bookmarks', JSON.stringify(bookmark), {headers: this.headers, observe: 'response'})
+      .post(this.baseUrl + userId + '/codingmarks', JSON.stringify(bookmark), {headers: this.headers, observe: 'response'})
       .pipe(shareReplay());
   }
 
