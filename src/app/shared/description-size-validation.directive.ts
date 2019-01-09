28746f8d9a55d83fe503a43da83b4b52e45666ff
Adrian Matei <ama@codepedia.org>
2019-01-09 21:25:44 +0100

    add description validation with error messages by codingmark creation

---
+++ src/app/shared/description-size-validation.directive.ts
@@ -0,0 +1,22 @@
+import {Directive} from '@angular/core';
+import {AbstractControl, FormArray, NG_VALIDATORS, ValidationErrors, Validator, ValidatorFn} from '@angular/forms';
+
+export const descriptionSizeValidator: ValidatorFn = (control: FormArray): ValidationErrors | null => {
+  const maxNumberOfCharacters = 1000;
+  const maxNumberOfLines = 21;
+  const numberOfLines = control.value.split('\n').length;
+  const numberOfCharacters = control.value.length;
+  const validationResponse = numberOfLines > maxNumberOfLines ? {'tooManyLines': {value: numberOfLines}} : numberOfCharacters > maxNumberOfCharacters ? {'tooManyCharacters': {value: numberOfCharacters}} : null;
+
+  return validationResponse;
+};
+
+@Directive({
+  selector: '[appDescriptionSizeValidator]',
+  providers: [{ provide: NG_VALIDATORS, useExisting: DescriptionSizeValidatorDirective, multi: true }]
+})
+export class DescriptionSizeValidatorDirective implements Validator {
+  validate(control: AbstractControl): ValidationErrors {
+    return descriptionSizeValidator(control);
+  }
+}
