016ebb37be118816af5a59ec436df4238e7e9bf5
Adrian Matei <ama@codepedia.org>
2019-01-21 21:20:33 +0100

    extend public codingmarks integration tests

--- routes/public-codingmarks.integration-test.js
+++ routes/public-codingmarks.integration-test.js
@@ -4,31 +4,33 @@ var request = require('supertest');
 var HttpStatus = require('http-status-codes');
 var expect = chai.expect;
 
-describe('Public API Tests', function() {
-  it('should return the latest codingmarks - without query parameters', function(done) {
-    request(app)
-      .get('/api/public/codingmarks/latest-entries')
-      .end(function(err, res) {
-        expect(res.statusCode).to.equal(200);
-        done();
-      });
-  });
+describe('Public API Tests', function () {
 
- it('should return the latest codingmarks - "since" parameter', function(done) {
-    let oneMonthBeforeNow = new Date();
-    oneMonthBeforeNow.setMonth(oneMonthBeforeNow.getMonth()-1);
-    request(app)
-      .get('/api/public/codingmarks/latest-entries')
-      .query({since: oneMonthBeforeNow.getTime()})
-      .end(function(err, res) {
-        //expect(res.statusCode).to.be.equal.to(200);
-        expect(res.statusCode).to.equal(200);
-        //expect(res.statusCode).to.equal(HttpStatus.OK);
-        done();
-      });
-  });
+  describe('get latest codingmarks function tests', function () {
+    const latestEntriesApiBaseUrl = '/api/public/codingmarks/latest-entries';
 
-  it('should return the latest codingmarks - "since" and "to" parameter', function(done) {
+    it('should return the latest codingmarks - without query parameters', function (done) {
+      request(app)
+        .get(latestEntriesApiBaseUrl)
+        .end(function (err, res) {
+          expect(res.statusCode).to.equal(HttpStatus.OK);
+          done();
+        });
+    });
+
+    it('should return the latest codingmarks - "since" parameter', function (done) {
+      let oneMonthBeforeNow = new Date();
+      oneMonthBeforeNow.setMonth(oneMonthBeforeNow.getMonth() - 1);
+      request(app)
+        .get(latestEntriesApiBaseUrl)
+        .query({since: oneMonthBeforeNow.getTime()})
+        .end(function (err, res) {
+          expect(res.statusCode).to.equal(HttpStatus.OK);
+          done();
+        });
+    });
+
+    it('should return the latest codingmarks - "since" and "to" parameter', function (done) {
       let sevenDaysBeforeNow = new Date();
       sevenDaysBeforeNow.setDate(sevenDaysBeforeNow.getDate() - 7);
 
@@ -36,16 +38,16 @@ describe('Public API Tests', function() {
       twoDaysBeforeNow.setDate(twoDaysBeforeNow.getDate() - 7);
 
       request(app)
-        .get('/api/public/codingmarks/latest-entries')
+        .get(latestEntriesApiBaseUrl)
         .query({since: sevenDaysBeforeNow.getTime()})
         .query({to: twoDaysBeforeNow.getTime()})
-        .end(function(err, res) {
+        .end(function (err, res) {
           expect(res.statusCode).to.equal(HttpStatus.OK);
           done();
         });
     });
 
-    it('should return bad request - invalid "since" and "to" parameter', function(done) {
+    it('should return bad request - invalid "since" and "to" parameter', function (done) {
       let sevenDaysBeforeNow = new Date();
       sevenDaysBeforeNow.setDate(sevenDaysBeforeNow.getDate() - 7);
 
@@ -53,12 +55,59 @@ describe('Public API Tests', function() {
       twoDaysBeforeNow.setDate(twoDaysBeforeNow.getDate() - 2);
 
       request(app)
-        .get('/api/public/codingmarks/latest-entries')
+        .get(latestEntriesApiBaseUrl)
         .query({since: twoDaysBeforeNow.getTime()})
         .query({to: sevenDaysBeforeNow.getTime()})
-        .end(function(err, res) {
+        .end(function (err, res) {
           expect(res.statusCode).to.equal(HttpStatus.BAD_REQUEST);
           done();
         });
     });
+  });
+
+  describe('get public codingmarks root endpoint tests', function () {
+    const publicCodingmarksApiBaseUrl = '/api/public/codingmarks';
+
+    it('should return all public codingmarks', function (done) {
+      request(app)
+        .get(publicCodingmarksApiBaseUrl)
+        .end(function (err, res) {
+          expect(res.statusCode).to.equal(HttpStatus.OK);
+          done();
+        });
+    });
+
+    it('should find codingmark by location', function (done) {
+      request(app)
+        .get(publicCodingmarksApiBaseUrl)
+        .query({location: 'https://www.codingmarks.org'})
+        .end(function (err, res) {
+          expect(res.statusCode).to.equal(HttpStatus.OK);
+          done();
+        });
+    });
+
+    it('should not find codingmark by location', function (done) {
+      request(app)
+        .get(publicCodingmarksApiBaseUrl)
+        .query({location: 'unknown_url'})
+        .end(function (err, res) {
+          expect(res.statusCode).to.equal(HttpStatus.NOT_FOUND);
+          done();
+        });
+    });
+
+    it('should find codingmarks by tag', function (done) {
+      request(app)
+        .get(publicCodingmarksApiBaseUrl)
+        .query({tag: 'java'})
+        .end(function (err, res) {
+          expect(res.statusCode).to.equal(HttpStatus.OK);
+          done();
+        });
+    });
+
+
+  });
+
 });
--- routes/public-codingmarks.js
+++ routes/public-codingmarks.js
@@ -6,14 +6,46 @@ var Bookmark = require('../models/bookmark');
 var HttpStatus = require('http-status-codes');
 var MyError = require('../models/error');
 
+
+/**
+ *  Returns the public codingmarks
+ *  Currently only the get all (no filter) function is used
+ */
+router.get('/', async (req, res) => {
+  try {
+    if (req.query.term) {
+      var regExpTerm = new RegExp(req.query.term, 'i');
+      var regExpSearch = [{name: {$regex: regExpTerm}}, {description: {$regex: regExpTerm}}, {category: {$regex: regExpTerm}}, {tags: {$regex: regExpTerm}}];
+      const bookmarks = await Bookmark.find({'$or': regExpSearch})
+      res.send(bookmarks);
+    } else if (req.query.location) {
+      const bookmark = await Bookmark.findOne({'shared': true, location: req.query.location}).lean().exec();
+      if (!bookmark) {
+        return res.status(HttpStatus.NOT_FOUND).send("Codingmark not found");
+      }
+      res.send(bookmark);
+    } else if (req.query.tag) {//get all bookmarks tagged with "tag"
+      //const bookmarks = await Bookmark.find({ tags: { $regex: new RegExp(req.query.tag, "ig")} }).sort({createdAt: -1});
+      const bookmarks = await Bookmark.find({tags: req.query.tag}).sort({createdAt: -1}).lean().exec();
+      res.send(bookmarks);
+    } else {//no filter - all bookmarks ordered by creation date descending
+      const bookmarks = await Bookmark.find({'shared': true}).sort({createdAt: -1}).lean().exec();
+      res.send(bookmarks);
+    }
+  } catch (err) {
+    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err);
+  }
+
+});
+
 /* GET title of bookmark given its url */
-router.get('/scrape', function(req, res, next) {
-  if(req.query.url){
+router.get('/scrape', function (req, res, next) {
+  if (req.query.url) {
     request(req.query.url, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         const $ = cheerio.load(body);
         const webpageTitle = $("title").text();
-        const metaDescription =  $('meta[name=description]').attr("content");
+        const metaDescription = $('meta[name=description]').attr("content");
         const webpage = {
           title: webpageTitle,
           metaDescription: metaDescription
@@ -33,20 +65,18 @@ router.get('/scrape', function(req, res, next) {
  *
  */
 router.get('/latest-entries', async (req, res) => {
-  try
-  {
-
-    if(req.query.since) {
+  try {
+    if (req.query.since) {
       const fromDate = new Date(parseFloat(req.query.since, 0));
       const toDate = req.query.to ? new Date(parseFloat(req.query.to, 0)) : new Date();
-      if(fromDate > toDate) {
+      if (fromDate > toDate) {
         return res
           .status(HttpStatus.BAD_REQUEST)
           .send(new MyError('timing query parameters values', ['<Since> param value must be before <to> parameter value']));
       }
       const codingmarks = await Bookmark.find(
         {
-          'shared':true,
+          'shared': true,
           createdAt: {
             $gte: fromDate,
             $lte: toDate
@@ -60,28 +90,26 @@ router.get('/latest-entries', async (req, res) => {
 
       const codingmarks = await Bookmark.find(
         {
-          'shared':true,
-          createdAt: { $gte: new Date((new Date().getTime() - (numberOfDaysToLookBack * 24 * 60 * 60 * 1000))) }
+          'shared': true,
+          createdAt: {$gte: new Date((new Date().getTime() - (numberOfDaysToLookBack * 24 * 60 * 60 * 1000)))}
         }).sort({createdAt: 'desc'}).lean().exec();
 
       res.send(codingmarks);
     }
 
-  }
-  catch (err)
-  {
+  } catch (err) {
     return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err);
   }
 });
 
 /* GET bookmark by id. */
-router.get('/:id', function(req, res, next) {
-  Bookmark.findById(req.params.id, function(err, bookmark){
-    if(err){
+router.get('/:id', function (req, res, next) {
+  Bookmark.findById(req.params.id, function (err, bookmark) {
+    if (err) {
       console.log(err);
       return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err);
     }
-    if(!bookmark){
+    if (!bookmark) {
       return res.status(HttpStatus.NOT_FOUND).send("Codingmark not found");
     }
     res.send(bookmark);
@@ -89,65 +117,33 @@ router.get('/:id', function(req, res, next) {
 
 });
 
-/* GET bookmarks listing. */
-router.get('/', async (req, res) => {
-  console.log(module.filename);
-  try
-  {
-    if(req.query.term){
-      var regExpTerm = new RegExp(req.query.term, 'i');
-      var regExpSearch=[{name:{$regex:regExpTerm}}, {description:{$regex: regExpTerm }}, {category:{$regex:regExpTerm }}, {tags:{$regex:regExpTerm}}];
-      const bookmarks = await Bookmark.find({'$or':regExpSearch})
-      res.send(bookmarks);
-    } else if (req.query.location) {
-      const bookmark = await Bookmark.findOne({'shared':true, location: req.query.location}).lean().exec();
-      if(!bookmark){
-        return res.status(HttpStatus.NOT_FOUND).send("Codingmark not found");
-      }
-      res.send(bookmark);
-    } else if(req.query.tag){//get all bookmarks tagged with "tag"
-      //const bookmarks = await Bookmark.find({ tags: { $regex: new RegExp(req.query.tag, "ig")} }).sort({createdAt: -1});
-      const bookmarks = await Bookmark.find({ tags: req.query.tag }).sort({createdAt: -1}).lean().exec();
-      res.send(bookmarks);
-    } else {//no filter - all bookmarks ordered by creation date descending
-      const bookmarks = await Bookmark.find({'shared':true}).sort({createdAt: -1}).lean().exec();
-      res.send(bookmarks);
-    }
-  }
-  catch (err)
-  {
-    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err);
-  }
-
-});
-
 
 /* TODO - maybe implement later advancedSearch */
-router.get('/advanced-search', function(req, res, next) {
-  var regexSearch=[];
-  if(req.query.name){
+router.get('/advanced-search', function (req, res, next) {
+  var regexSearch = [];
+  if (req.query.name) {
     var regExpName = new RegExp(req.query.category, 'i');
-    regexSearch.push({ 'name': { $regex: regExpName }});
-    regexSearch.push({ 'description': { $regex: regExpName }});
+    regexSearch.push({'name': {$regex: regExpName}});
+    regexSearch.push({'description': {$regex: regExpName}});
   }
-  if(req.query.category){
+  if (req.query.category) {
     var regExpCategory = new RegExp(req.query.category, 'i');
-    regexSearch.push({ 'category': { $regex: regExpCategory }});
+    regexSearch.push({'category': {$regex: regExpCategory}});
   }
-  if(req.query.tag){
+  if (req.query.tag) {
     var regExpTag = new RegExp(req.query.tag, 'i');
-    regexSearch.push({ 'tags': { $regex: regExpTag }});
+    regexSearch.push({'tags': {$regex: regExpTag}});
   }
-  if(regexSearch.length > 0){
-    Bookmark.find().or(regexSearch, function(err, bookmarks){
-      if(err){
+  if (regexSearch.length > 0) {
+    Bookmark.find().or(regexSearch, function (err, bookmarks) {
+      if (err) {
         return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err);
       }
       res.send(bookmarks);
     });
   } else {//no filter - all bookmarks
-    Bookmark.find({}, function(err, bookmarks){
-      if(err){
+    Bookmark.find({}, function (err, bookmarks) {
+      if (err) {
         return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err);
       }
       res.send(bookmarks);
