41731ee7847c37308811fd55983c57231f539f98
Adrian Matei <ama@codingpedia.org>
2019-01-25 07:17:35 +0100

    extend integration tests with successful creation and deletion...

--- routes/personal-codingmarks.js
+++ routes/personal-codingmarks.js
@@ -11,12 +11,12 @@ var config = common.config();
 var HttpStatus = require('http-status-codes');
 
 //showdown converter - https://github.com/showdownjs/showdown
-var showdown  = require('showdown'),
-    converter = new showdown.Converter();
+var showdown = require('showdown'),
+  converter = new showdown.Converter();
 
 //add keycloak middleware
-var keycloak = new Keycloak({ scope: 'openid' }, config.keycloak);
-router.use( keycloak.middleware() );
+var keycloak = new Keycloak({scope: 'openid'}, config.keycloak);
+router.use(keycloak.middleware());
 
 const MAX_NUMBER_OF_TAGS = 8;
 
@@ -30,7 +30,7 @@ const MAX_NUMBER_OF_LINES_FOR_DESCRIPTION = 100;
 router.post('/:userId/codingmarks', keycloak.protect(), async (request, response) => {
 
   let userId = request.kauth.grant.access_token.content.sub;
-  if(userId !== request.params.userId) {
+  if (userId !== request.params.userId) {
     return response
       .status(HttpStatus.UNAUTHORIZED)
       .send(new MyError('Unauthorized', ['the userId does not match the subject in the access token']));
@@ -39,47 +39,47 @@ router.post('/:userId/codingmarks', keycloak.protect(), async (request, response
   const codingmark = buildCodingmarkFromRequest(request);
 
   const missingRequiredAttributes = !codingmark.name || !codingmark.location || !codingmark.tags || codingmark.tags.length === 0;
-  if(missingRequiredAttributes) {
+  if (missingRequiredAttributes) {
     return response
-              .status(HttpStatus.BAD_REQUEST)
-              .send(new MyError('Missing required attributes', ['Missing required attributes']));
+      .status(HttpStatus.BAD_REQUEST)
+      .send(new MyError('Missing required attributes', ['Missing required attributes']));
   }
-  if(codingmark.tags.length > MAX_NUMBER_OF_TAGS){
+  if (codingmark.tags.length > MAX_NUMBER_OF_TAGS) {
     return response
-              .status(HttpStatus.BAD_REQUEST)
-              .send(new MyError('Too many tags have been submitted', ['Too many tags have been submitted']));
+      .status(HttpStatus.BAD_REQUEST)
+      .send(new MyError('Too many tags have been submitted', ['Too many tags have been submitted']));
   }
 
   const descriptionIsTooLong = codingmark.description.length > MAX_NUMBER_OF_CHARS_FOR_DESCRIPTION;
-  if(descriptionIsTooLong) {
+  if (descriptionIsTooLong) {
     return response
       .status(HttpStatus.BAD_REQUEST)
       .send(new MyError('The description is too long. Only ' + MAX_NUMBER_OF_CHARS_FOR_DESCRIPTION + ' allowed', ['The description is too long. Only ' + MAX_NUMBER_OF_CHARS_FOR_DESCRIPTION + ' allowed']));
   }
 
-  if(codingmark.description){
+  if (codingmark.description) {
     const descriptionHasTooManyLines = codingmark.description.split('\n').length > MAX_NUMBER_OF_LINES_FOR_DESCRIPTION;
-    if(descriptionHasTooManyLines){
+    if (descriptionHasTooManyLines) {
       return response
         .status(HttpStatus.BAD_REQUEST)
         .send(new MyError('The description hast too many lines. Only ' + MAX_NUMBER_OF_LINES_FOR_DESCRIPTION + ' allowed', ['The description hast too many lines. Only ' + MAX_NUMBER_OF_LINES_FOR_DESCRIPTION + ' allowed']));
     }
   }
 
-  try{
+  try {
     let newBookmark = await codingmark.save();
 
     response
       .set('Location', `${config.basicApiUrl}private/${request.params.userId}/codingmarks/${newBookmark.id}`)
       .status(HttpStatus.CREATED)
-      .send({response:'Bookmark created for userId ' + request.params.userId});
+      .send({response: 'Bookmark created for userId ' + request.params.userId});
 
-  } catch (err){
+  } catch (err) {
     const duplicateKeyinMongoDb = err.name === 'MongoError' && err.code === 11000;
     if (duplicateKeyinMongoDb) {
-       return response
-                .status(HttpStatus.CONFLICT)
-                .send(new MyError('Duplicate key', [err.message]));
+      return response
+        .status(HttpStatus.CONFLICT)
+        .send(new MyError('Duplicate key', [err.message]));
     }
     response
       .status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -89,7 +89,7 @@ router.post('/:userId/codingmarks', keycloak.protect(), async (request, response
 });
 
 let buildCodingmarkFromRequest = function (req) {
-  const descriptionHtml = req.body.descriptionHtml ? req.body.descriptionHtml: converter.makeHtml(req.body.description);
+  const descriptionHtml = req.body.descriptionHtml ? req.body.descriptionHtml : converter.makeHtml(req.body.description);
   const bookmark = new Bookmark({
     name: req.body.name,
     location: req.body.location,
@@ -109,29 +109,64 @@ let buildCodingmarkFromRequest = function (req) {
   return bookmark;
 };
 
-/* GET bookmarks for user */
+/* GET personal codingmarks of the user */
 router.get('/:userId/codingmarks', keycloak.protect(), async (request, response) => {
-  try{
+  try {
     let codingmarks;
     let userId = request.kauth.grant.access_token.content.sub;
-    if(userId !== request.params.userId) {
+    if (userId !== request.params.userId) {
       return response
         .status(HttpStatus.UNAUTHORIZED)
         .send(new MyError('Unauthorized', ['the userId does not match the subject in the access token']));
     }
-    if(request.query.term){
+    if (request.query.term) {
       var regExpTerm = new RegExp(request.query.term, 'i');
-      var regExpSearch=[{name:{$regex:regExpTerm}}, {description:{$regex: regExpTerm }}, {category:{$regex:regExpTerm }}, {tags:{$regex:regExpTerm}}];
-      codingmarks = await Bookmark.find({userId:request.params.userId, '$or':regExpSearch});
+      var regExpSearch = [{name: {$regex: regExpTerm}}, {description: {$regex: regExpTerm}}, {category: {$regex: regExpTerm}}, {tags: {$regex: regExpTerm}}];
+      codingmarks = await Bookmark.find({userId: request.params.userId, '$or': regExpSearch});
     } else {//no filter - all bookmarks
-      codingmarks = await Bookmark.find({userId:request.params.userId});
+      codingmarks = await Bookmark.find({userId: request.params.userId});
     }
 
     response.send(codingmarks);
   } catch (err) {
     return response
-            .status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .send(err);
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .send(err);
+  }
+});
+
+/* GET codingmark of user */
+router.get('/:userId/codingmarks/:codingmarkId', keycloak.protect(), async (request, response) => {
+
+  const userId = request.kauth.grant.access_token.content.sub;
+  if (userId !== request.params.userId) {
+    return response
+      .status(HttpStatus.UNAUTHORIZED)
+      .send(new MyError('Unauthorized', ['the userId does not match the subject in the access token']));
+  }
+
+  try {
+    const codingmark = await Bookmark.findOne({
+      _id: request.params.codingmarkId,
+      userId: request.params.userId
+    });
+
+    if (!codingmark) {
+      return response
+        .status(HttpStatus.NOT_FOUND)
+        .send(new MyError(
+          'Not Found Error',
+          ['Bookmark for user id ' + request.params.userId + ' and bookmark id ' + request.params.codingmarkId + ' not found']
+          )
+        );
+    } else {
+      response.status(HttpStatus.OK).send(codingmark);
+    }
+  } catch (err) {
+    return response
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .send(new MyError('Unknown server error',
+        ['Unknown server error when trying to delete codingmark with id ' + request.params.codingmarkId]));
   }
 });
 
@@ -142,54 +177,57 @@ router.get('/:userId/codingmarks', keycloak.protect(), async (request, response)
 router.put('/:userId/codingmarks/:codingmarkId', keycloak.protect(), async (request, response) => {
 
   let userId = request.kauth.grant.access_token.content.sub;
-  if(userId !== request.params.userId) {
+  if (userId !== request.params.userId) {
     return response
       .status(HttpStatus.UNAUTHORIZED)
       .send(new MyError('Unauthorized', ['the userId does not match the subject in the access token']));
   }
 
   const requiredAttributesMissing = !request.body.name || !request.body.location || !request.body.tags || request.body.tags.length === 0;
-  if(requiredAttributesMissing){
+  if (requiredAttributesMissing) {
     return response
-              .status(HttpStatus.BAD_REQUEST)
-              .send(new MyError('Missing required attributes', ['Missing required attributes']));
+      .status(HttpStatus.BAD_REQUEST)
+      .send(new MyError('Missing required attributes', ['Missing required attributes']));
   }
 
-  if(request.body.tags.length > MAX_NUMBER_OF_TAGS){
+  if (request.body.tags.length > MAX_NUMBER_OF_TAGS) {
     return response
-              .status(HttpStatus.BAD_REQUEST)
-              .send(new MyError('Too many tags have been submitted', ['Too many tags have been submitted']));
+      .status(HttpStatus.BAD_REQUEST)
+      .send(new MyError('Too many tags have been submitted', ['Too many tags have been submitted']));
   }
 
   const descriptionIsTooLong = request.body.description.length > MAX_NUMBER_OF_CHARS_FOR_DESCRIPTION;
-  if(descriptionIsTooLong) {
+  if (descriptionIsTooLong) {
     return response
       .status(HttpStatus.BAD_REQUEST)
       .send(new MyError('The description is too long. Only ' + MAX_NUMBER_OF_CHARS_FOR_DESCRIPTION + ' allowed',
-                        ['The description is too long. Only ' + MAX_NUMBER_OF_CHARS_FOR_DESCRIPTION + ' allowed']));
+        ['The description is too long. Only ' + MAX_NUMBER_OF_CHARS_FOR_DESCRIPTION + ' allowed']));
   }
 
-  if(request.body.description){
+  if (request.body.description) {
     const descriptionHasTooManyLines = request.body.description.split('\n').length > MAX_NUMBER_OF_LINES_FOR_DESCRIPTION;
-    if(descriptionHasTooManyLines){
+    if (descriptionHasTooManyLines) {
       return response
         .status(HttpStatus.BAD_REQUEST)
         .send(new MyError('The description hast too many lines. Only ' + MAX_NUMBER_OF_LINES_FOR_DESCRIPTION + ' allowed',
-                          ['The description hast too many lines. Only ' + MAX_NUMBER_OF_LINES_FOR_DESCRIPTION + ' allowed']));
+          ['The description hast too many lines. Only ' + MAX_NUMBER_OF_LINES_FOR_DESCRIPTION + ' allowed']));
     }
   }
 
-  if(!request.body.descriptionHtml){
+  if (!request.body.descriptionHtml) {
     request.body.descriptionHtml = converter.makeHtml(request.body.description);
   }
   try {
-    const codingmark = await Bookmark.findOneAndUpdate({_id: request.params.codingmarkId, userId: request.params.userId}, request.body, {new: true});
+    const codingmark = await Bookmark.findOneAndUpdate({
+      _id: request.params.codingmarkId,
+      userId: request.params.userId
+    }, request.body, {new: true});
 
     const codingmarkNotFound = !codingmark;
     if (codingmarkNotFound) {
       return response
         .status(HttpStatus.NOT_FOUND)
-        .send(new MyError('Not Found Error', ['Codingmark for user id ' + request.params.userId + ' and codingmark id '+ request.params.codingmarkId + ' not found']));
+        .send(new MyError('Not Found Error', ['Codingmark for user id ' + request.params.userId + ' and codingmark id ' + request.params.codingmarkId + ' not found']));
     } else {
       response
         .status(200)
@@ -198,12 +236,12 @@ router.put('/:userId/codingmarks/:codingmarkId', keycloak.protect(), async (requ
   } catch (err) {
     if (err.name === 'MongoError' && err.code === 11000) {
       return response
-                .status(HttpStatus.CONFLICT)
-                .send(new MyError('Duplicate key', [err.message]));
+        .status(HttpStatus.CONFLICT)
+        .send(new MyError('Duplicate key', [err.message]));
     }
     return response
       .status(HttpStatus.INTERNAL_SERVER_ERROR)
-      .send(new MyError('Unknown Server Error', ['Unknown server error when updating codingmark for user id ' + request.params.userId + ' and codingmark id '+ request.params.codingmarkId]));
+      .send(new MyError('Unknown Server Error', ['Unknown server error when updating codingmark for user id ' + request.params.userId + ' and codingmark id ' + request.params.codingmarkId]));
   }
 });
 
@@ -213,31 +251,34 @@ router.put('/:userId/codingmarks/:codingmarkId', keycloak.protect(), async (requ
 router.delete('/:userId/codingmarks/:codingmarkId', keycloak.protect(), async (request, response) => {
 
   const userId = request.kauth.grant.access_token.content.sub;
-  if(userId !== request.params.userId) {
+  if (userId !== request.params.userId) {
     return response
       .status(HttpStatus.UNAUTHORIZED)
       .send(new MyError('Unauthorized', ['the userId does not match the subject in the access token']));
   }
 
   try {
-    const codingmark = await Bookmark.findOneAndRemove({_id: request.params.codingmarkId, userId: request.params.userId});
+    const codingmark = await Bookmark.findOneAndRemove({
+      _id: request.params.codingmarkId,
+      userId: request.params.userId
+    });
 
     if (!codingmark) {
       return response
-                .status(HttpStatus.NOT_FOUND)
-                .send(new MyError(
-                                  'Not Found Error',
-                                  ['Bookmark for user id ' + request.params.userId + ' and bookmark id '+ request.params.codingmarkId + ' not found']
-                                )
-                );
+        .status(HttpStatus.NOT_FOUND)
+        .send(new MyError(
+          'Not Found Error',
+          ['Bookmark for user id ' + request.params.userId + ' and bookmark id ' + request.params.codingmarkId + ' not found']
+          )
+        );
     } else {
       response.status(HttpStatus.NO_CONTENT).send('Codingmark successfully deleted');
     }
   } catch (err) {
     return response
-            .status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .send(new MyError('Unknown server error',
-                              ['Unknown server error when trying to delete codingmark with id ' + request.params.codingmarkId]));
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .send(new MyError('Unknown server error',
+        ['Unknown server error when trying to delete codingmark with id ' + request.params.codingmarkId]));
   }
 });
 
