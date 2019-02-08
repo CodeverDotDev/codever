41448f0b6ebe2fc94c78a75529bbe76a8345b695
Adrian Matei <ama@codepedia.org>
2019-02-08 21:06:33 +0100

    repare wrong commit for environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
-    production: false,
-    APP_HOME_URL: 'http://localhost:4200',
-    API_URL: 'https://www.codingmarks.org/api',
-    HOST: 'https://www.codingmarks.org/',
-    keycloak:  {
-        'realm': 'codingpedia',
-        'url': 'https://www.codingmarks.org/auth',
-        'clientId': 'codingmarks'
-    }
+  production: false,
+  APP_HOME_URL: 'http://localhost:4200',
+  API_URL: 'http://localhost:3000/api',
+  HOST: 'http://localhost:4200/',
+  keycloak:  {
+    'realm': 'codingpedia',
+    'url': 'http://localhost:8380/auth',
+    'clientId': 'codingmarks'
+  }
 };
