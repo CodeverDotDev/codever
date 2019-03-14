8ac06b03560f1707af3a137a8d35de205cc5bb76
Adrian Matei <ama@codingpedia.org>
2019-03-14 06:48:25 +0100

    use boundary by characters when filtering bookmarks

--- src/app/core/filter.service.ts
+++ src/app/core/filter.service.ts
@@ -1,207 +1,203 @@
-import { Injectable } from '@angular/core';
-import { Codingmark } from './model/codingmark';
-import { List } from 'immutable';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {Codingmark} from './model/codingmark';
+import {List} from 'immutable';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class BookmarkFilterService {
 
-    /**
-     * Filters a list of codingmarks based on the query string.
-     *
-     * Tags are enclosed in square brackets - e.g [angular]. The filter is now permissive, that is when starting with
-     * "[" the filter assumes that the tag is what comes after even though there is no enclosing "]". That is now to support
-     * the autosearch feature
-     *
-     * @param query - is a string of search terms; multiple terms are separated via the "+" sign
-     * @param bookmarks$ - the list to be filtered
-     * @returns {any} - the filtered list
-     */
-    filterBookmarksBySearchTerm(query: string, language: string, bookmarks$: Observable<List<Codingmark>>): Codingmark[] {
+  /**
+   * Filters a list of bookmarks based on the query string.
+   *
+   * Tags are enclosed in square brackets - e.g [angular]. The filter is now permissive, that is when starting with
+   * "[" the filter assumes that the tag is what comes after even though there is no enclosing "]". That is now to support
+   * the autosearch feature
+   *
+   * @param query - is a string of search terms; multiple terms are separated via the "+" sign
+   * @param bookmarks$ - the list to be filtered
+   * @returns {any} - the filtered list
+   */
+  filterBookmarksBySearchTerm(query: string, language: string, bookmarks$: Observable<List<Codingmark>>): Codingmark[] {
 
-        const searchedTermsAndTags: [string[], string[]] = this.splitSearchQuery(query);
-        const searchedTerms: string[] = searchedTermsAndTags[0];
-        const searchedTags: string[] = searchedTermsAndTags[1];
-        let result: Codingmark[] = [];
+    const searchedTermsAndTags: [string[], string[]] = this.splitSearchQuery(query);
+    const searchedTerms: string[] = searchedTermsAndTags[0];
+    const searchedTags: string[] = searchedTermsAndTags[1];
+    let result: Codingmark[] = [];
 
-        bookmarks$.subscribe(
-            bookmarks => {
-                let filteredBookmarks = bookmarks.toArray(); // we start with all bookmarks
-                if (language && language !== 'all') {
-                    filteredBookmarks = filteredBookmarks.filter(x => x.language === language);
-                }
-                searchedTags.forEach(tag => {
-                    filteredBookmarks = filteredBookmarks.filter(x => this.bookmarkContainsTag(x, tag));
-                });
-                searchedTerms.forEach(term => {
-                    filteredBookmarks = filteredBookmarks.filter(x => this.bookmarkContainsSearchedTerm(x, term.trim()));
-                });
-
-                result = filteredBookmarks;
-            },
-            err => {
-                console.log('Error filtering bookmakrs');
-            }
-        );
-
-        return result;
-    }
-
-    /**
-     * It will parse the search query and returns the search terms and tags to filter.
-     * It is permissive, in the sense that "[angul" is seen as the "angul" tag - needed for autocomplete
-     * To see what should come out check the filter.service.spec.ts test examples
-     *
-     * @param query to be parsed
-     * @returns a tuple of terms (first element) and tags (second element)
-     */
-    public splitSearchQuery(query: string): [string[], string[]] {
-
-        const result: [string[], string[]] = [[], []];
-
-        const terms: string[] = [];
-        let term = '';
-        const tags: string[] = [];
-        let tag = '';
-
-        let isInsideTerm = false;
-        let isInsideTag = false;
-
-
-        for (let i = 0; i < query.length; i++) {
-            const currentCharacter = query[i];
-            if (currentCharacter === ' ') {
-                if (!isInsideTag) {
-                    if (!isInsideTerm) {
-                        continue;
-                    } else {
-                        terms.push(term);
-                        isInsideTerm = false;
-                        term = '';
-                    }
-                } else {
-                    tag += ' ';
-                }
-            } else if (currentCharacter === '[') {
-                if (isInsideTag) {
-                    tags.push(tag.trim());
-                    tag = '';
-                } else {
-                    isInsideTag = true;
-                }
-            } else if (currentCharacter === ']') {
-                if (isInsideTag) {
-                    isInsideTag = false;
-                    tags.push(tag.trim());
-                    tag = '';
-                }
-            } else {
-                if (isInsideTag) {
-                    tag += currentCharacter;
-                } else {
-                    isInsideTerm = true;
-                    term += currentCharacter;
-                }
-            }
+    bookmarks$.subscribe(
+      bookmarks => {
+        let filteredBookmarks = bookmarks.toArray(); // we start with all bookmarks
+        if (language && language !== 'all') {
+          filteredBookmarks = filteredBookmarks.filter(x => x.language === language);
         }
-
-        if (tag.length > 0) {
-            tags.push(tag.trim());
-        }
-
-        if (term.length > 0) {
-            terms.push(term);
-        }
-
-        result[0] = terms;
-        result[1] = tags;
-
-        return result;
-    }
-
-    /**
-     * Checks if one search term is present in the codingmark's metadata (name, location, description, tags)
-     * There is still an internal debate to use the contains method (less restrictive) and the
-     * RegExp with matching words (more restrictive and does not support propery Unicode)
-     *
-     * @param codingmark
-     * @param searchedTerm
-     * @returns {boolean}
-     */
-    private bookmarkContainsSearchedTerm(codingmark: Codingmark, searchedTerm: string): boolean {
-        let result = false;
-        const escapedSearchPattern = '\\b' + this.escapeRegExp(searchedTerm.toLowerCase()) + '\\b'
-        // const escapedSearchPattern = this.escapeRegExp(searchedTerm.toLowerCase()) + '[^\\w]'; //not working java javascript
-        const pattern = new RegExp(escapedSearchPattern);
-        // const pattern = new RegExp('\\b' + searchedTerm.toLowerCase() + '\\b');
-        /*    if (codingmark.name.toLowerCase().indexOf(term.toLowerCase()) !== -1
-              || codingmark.location.toLowerCase().indexOf(term.toLowerCase()) !== -1
-              || codingmark.description.toLowerCase().indexOf(term.toLowerCase()) !== -1
-              || codingmark.tags.indexOf(term.toLowerCase()) !== -1
-            ) {*/
-        if ((codingmark.name && pattern.test(codingmark.name.toLowerCase()))
-            // || (codingmark.location && pattern.test(codingmark.location.toLowerCase()))
-            || (codingmark.location.toLowerCase().indexOf(searchedTerm.toLowerCase()) !== -1) // enables search of entire url "/" is not caught in regex as not word character
-            || (codingmark.description && pattern.test(codingmark.description.toLowerCase()))
-        ) {
-            result = true;
-        }
-
-        if (result) {
-            return true;
-        } else {
-            // if not found already look through the tags also
-            codingmark.tags.forEach(tag => {
-                if (pattern.test(tag.toLowerCase())) {
-                    result = true;
-                }
-            });
-        }
-
-        return result;
-    }
-
-    /**
-     * It must be an exact match
-     * @param bookmark
-     * @param tag
-     */
-    private bookmarkContainsTag(bookmark: Codingmark, tag: string): boolean {
-        let result = false;
-
-        const escapedString = this.escapeRegExp(tag.toLowerCase());
-        bookmark.tags.forEach(bookmarkTag => {
-            if (bookmarkTag.toLowerCase() === tag.toLowerCase()) {
-                result = true;
-            }
+        searchedTags.forEach(tag => {
+          filteredBookmarks = filteredBookmarks.filter(x => this.bookmarkContainsTag(x, tag));
+        });
+        searchedTerms.forEach(term => {
+          filteredBookmarks = filteredBookmarks.filter(x => this.bookmarkContainsSearchedTerm(x, term.trim()));
         });
 
-        return result;
+        result = filteredBookmarks;
+      },
+      err => {
+        console.log('Error filtering bookmakrs');
+      }
+    );
+
+    return result;
+  }
+
+  /**
+   * It will parse the search query and returns the search terms and tags to filter.
+   * It is permissive, in the sense that "[angul" is seen as the "angul" tag - needed for autocomplete
+   * To see what should come out check the filter.service.spec.ts test examples
+   *
+   * @param query to be parsed
+   * @returns a tuple of terms (first element) and tags (second element)
+   */
+  public splitSearchQuery(query: string): [string[], string[]] {
+
+    const result: [string[], string[]] = [[], []];
+
+    const terms: string[] = [];
+    let term = '';
+    const tags: string[] = [];
+    let tag = '';
+
+    let isInsideTerm = false;
+    let isInsideTag = false;
+
+
+    for (let i = 0; i < query.length; i++) {
+      const currentCharacter = query[i];
+      if (currentCharacter === ' ') {
+        if (!isInsideTag) {
+          if (!isInsideTerm) {
+            continue;
+          } else {
+            terms.push(term);
+            isInsideTerm = false;
+            term = '';
+          }
+        } else {
+          tag += ' ';
+        }
+      } else if (currentCharacter === '[') {
+        if (isInsideTag) {
+          tags.push(tag.trim());
+          tag = '';
+        } else {
+          isInsideTag = true;
+        }
+      } else if (currentCharacter === ']') {
+        if (isInsideTag) {
+          isInsideTag = false;
+          tags.push(tag.trim());
+          tag = '';
+        }
+      } else {
+        if (isInsideTag) {
+          tag += currentCharacter;
+        } else {
+          isInsideTerm = true;
+          term += currentCharacter;
+        }
+      }
     }
 
-    private escapeRegExp(str): string {
-        const specials = [
-                // order matters for these
-                '-'
-                , '['
-                , ']'
-                // order doesn't matter for any of these
-                , '/'
-                , '{'
-                , '}'
-                , '('
-                , ')'
-                , '*'
-                , '+'
-                , '?'
-                , '.'
-                , '\\'
-                , '^'
-                , '$'
-                , '|'
-                , '/'
-            ],
-            regex = RegExp('[' + specials.join('\\') + ']', 'g');
-        return str.replace(regex, '\\$&'); // $& means the whole matched string
+    if (tag.length > 0) {
+      tags.push(tag.trim());
     }
+
+    if (term.length > 0) {
+      terms.push(term);
+    }
+
+    result[0] = terms;
+    result[1] = tags;
+
+    return result;
+  }
+
+  /**
+   * Checks if one search term is present in the bookmark's metadata (name, location, description, tags)
+   * There is still an internal debate to use the contains(includes) vs word boundary or characters boundary (current)
+   * See if there are performance issues
+   *
+   * @param bookmark
+   * @param searchedTerm
+   * @returns {boolean}
+   */
+  private bookmarkContainsSearchedTerm(bookmark: Codingmark, searchedTerm: string): boolean {
+    let result = false;
+    // const escapedSearchPattern = '\\b' + this.escapeRegExp(searchedTerm.toLowerCase()) + '\\b'; word boundary was not enough, especially for special characters which can happen in coding
+    // https://stackoverflow.com/questions/23458872/javascript-regex-word-boundary-b-issue
+    const separatingChars = '\\s\\.,;#\\-\\/_\\[\\]\\(\\)\\*\\+';
+    const escapedSearchPattern = `(^|[${separatingChars}])(${this.escapeRegExp(searchedTerm.toLowerCase())})(?=$|[${separatingChars}])`;
+    const pattern = new RegExp(escapedSearchPattern);
+    if ((bookmark.name && pattern.test(bookmark.name.toLowerCase()))
+      || (bookmark.location && pattern.test(bookmark.location.toLowerCase()))
+      || (bookmark.description && pattern.test(bookmark.description.toLowerCase()))
+    ) {
+      result = true;
+    }
+
+    if (result) {
+      return true;
+    } else {
+      // if not found already look through the tags also
+      bookmark.tags.forEach(tag => {
+        if (pattern.test(tag.toLowerCase())) {
+          result = true;
+        }
+      });
+    }
+
+    return result;
+  }
+
+  /**
+   * It must be an exact match
+   * @param bookmark
+   * @param tag
+   */
+  private bookmarkContainsTag(bookmark: Codingmark, tag: string): boolean {
+    let result = false;
+
+    const escapedString = this.escapeRegExp(tag.toLowerCase());
+    bookmark.tags.forEach(bookmarkTag => {
+      if (bookmarkTag.toLowerCase() === tag.toLowerCase()) {
+        result = true;
+      }
+    });
+
+    return result;
+  }
+
+  // TODO considering how often these characters might get used in search maybe is not important to escape them after all
+  private escapeRegExp(str): string {
+    const specials = [
+        // order matters for these
+        '-'
+        , '['
+        , ']'
+        // order doesn't matter for any of these
+        , '/'
+        , '{'
+        , '}'
+        , '('
+        , ')'
+        , '*'
+        , '+'
+        , '?'
+        , '.'
+        , '\\'
+        , '^'
+        , '$'
+        , '|'
+        , '/'
+      ],
+      regex = RegExp('[' + specials.join('\\') + ']', 'g');
+    return str.replace(regex, '\\$&'); // $& means the whole matched string
+  }
 }
