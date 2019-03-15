92f6b69bcbd3b629be73032a3037441de706e927
Adrian Matei <ama@codepedia.org>
2019-03-15 13:50:05 +0100

    route to tags component if only one word after the host

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,8 +1,12 @@
-import {RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes, UrlSegment } from '@angular/router';
 import {NgModule} from '@angular/core';
 import {TagComponent} from './tag/tag.component';
 import {AboutComponent} from './about/about.component';
 import {PublicCodingmarksComponent} from './codingmark/public-codingmarks.component';
+
+export function possibleTag(url: UrlSegment[]) {
+    return url.length === 1 && url[0].path !== 'personal' ? ({consumed: url}) : null;
+}
 
 const publicBookmarksRoutes: Routes = [
   {
@@ -22,7 +26,7 @@ const publicBookmarksRoutes: Routes = [
     path: '',
     component: PublicCodingmarksComponent
   },
-  { path: '**', component: TagComponent }
+  { matcher: possibleTag, component: TagComponent }
 ];
 @NgModule({
   imports: [RouterModule.forChild(publicBookmarksRoutes)],
--- src/app/public/tag/tag.component.ts
+++ src/app/public/tag/tag.component.ts
@@ -1,60 +1,55 @@
-
-import {map} from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
-import {TagService} from './tag.service';
-import {ActivatedRoute} from '@angular/router';
-import {Codingmark} from '../../core/model/codingmark';
-import {Observable} from 'rxjs';
-import {UserDataStore} from '../../core/user/userdata.store';
-import {KeycloakService} from 'keycloak-angular';
-import {UserData} from '../../core/model/user-data';
+import { TagService } from './tag.service';
+import { ActivatedRoute } from '@angular/router';
+import { Codingmark } from '../../core/model/codingmark';
+import { Observable } from 'rxjs';
+import { UserDataStore } from '../../core/user/userdata.store';
+import { KeycloakService } from 'keycloak-angular';
+import { UserData } from '../../core/model/user-data';
 
 @Component({
-  selector: 'app-tag',
-  templateUrl: './tag.component.html',
-  styleUrls: ['./tag.component.css']
+    selector: 'app-tag',
+    templateUrl: './tag.component.html',
+    styleUrls: ['./tag.component.css']
 })
 export class TagComponent implements OnInit {
 
-  bookmarksForTag$: Observable<Codingmark[]>;
-  tag: string;
-  userData: UserData;
-  counter = 30;
+    bookmarksForTag$: Observable<Codingmark[]>;
+    tag: string;
+    userData: UserData;
+    counter = 30;
 
-  constructor(private tagService: TagService,
-              private userDataStore: UserDataStore,
-              private keycloakService: KeycloakService,
-              private route: ActivatedRoute) { }
-
-  ngOnInit() {
-    this.tag = this.route.snapshot.params['tag'];
-    if (this.tag) {
-      this.bookmarksForTag$ = this.tagService.getBookmarksForTag(this.tag);
-    } else if (this.route.snapshot.url.length > 1) {
-      console.log('page not found');
-
-    } else {
-      this.tag = this.route.snapshot.url[0].path;
-      this.bookmarksForTag$ = this.tagService.getBookmarksForTag(this.tag);
+    constructor(private tagService: TagService,
+                private userDataStore: UserDataStore,
+                private keycloakService: KeycloakService,
+                private route: ActivatedRoute) {
     }
 
+    ngOnInit() {
+        this.tag = this.route.snapshot.params['tag'];
+        if (this.tag) {
+            this.bookmarksForTag$ = this.tagService.getBookmarksForTag(this.tag);
+        } else {
+            this.tag = this.route.snapshot.url[0].path;
+            this.bookmarksForTag$ = this.tagService.getBookmarksForTag(this.tag);
+        }
 
-    this.keycloakService.isLoggedIn().then(isLoggedIn => {
-      if (isLoggedIn) {
-        this.keycloakService.loadUserProfile().then(keycloakProfile => {
-          this.userDataStore.getUserData().subscribe(data => {
-              this.userData = data;
-            },
-            error => {
+        this.keycloakService.isLoggedIn().then(isLoggedIn => {
+            if (isLoggedIn) {
+                this.keycloakService.loadUserProfile().then(keycloakProfile => {
+                    this.userDataStore.getUserData().subscribe(data => {
+                            this.userData = data;
+                        },
+                        error => {
+                        }
+                    );
+                });
             }
-          );
         });
-      }
-    });
-  }
+    }
 
-  showMoreResults() {
-    this.counter += 10;
-  }
+    showMoreResults() {
+        this.counter += 10;
+    }
 
 }
