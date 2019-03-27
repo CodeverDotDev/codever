0a0231adf896dfc161be63ae940b829dde2ac3b6
Adrian Matei <ama@codingpedia.org>
2019-03-27 08:52:31 +0100

    create index with name

---
+++ resources/db-migration/mongodb/1553405821919_add_unique_userid_index.js
@@ -0,0 +1,13 @@
+
+db.users.createIndex(
+    {
+        userId:1
+    },
+    {
+        unique: true,
+        name: "unique_userId"
+    }
+)
+
+//show indexes (verify is present)
+db.users.getIndexes()
