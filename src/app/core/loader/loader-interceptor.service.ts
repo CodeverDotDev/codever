ade30169d0f4b55f5d7f85bb48c2f36231a5138c
Adrian Matei <ama@codingpedia.org>
2019-04-01 20:44:30 +0200

    add silentApis to loading interceptor to ignore scrape calls

--- src/app/core/loader/loader-interceptor.service.ts
+++ src/app/core/loader/loader-interceptor.service.ts
@@ -3,16 +3,26 @@ import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/com
 import {Observable} from 'rxjs';
 import {finalize} from 'rxjs/operators';
 import {LoaderService} from './loader.service';
+import {environment} from '../../../environments/environment';
 
 @Injectable()
 export class LoaderInterceptorService implements HttpInterceptor {
 
   private _inProgressCount = 0; // counter for multiple http requests
 
+  private readonly silentApis = [
+    environment.API_URL + '/public/bookmarks/scrape'
+  ];
   constructor(private loaderService: LoaderService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (req.method === 'GET') {
+
+      // Ignore silent api requests
+      if (this.checkUrl(req)) {
+        return next.handle(req);
+      }
+
       this._inProgressCount++;
       this.loaderService.show();
       return next.handle(req).pipe(
@@ -26,6 +36,16 @@ export class LoaderInterceptorService implements HttpInterceptor {
     } else {
       return next.handle(req);
     }
+  }
+
+  /**
+   * Check if request is silent.
+   * @param req request
+   */
+  private checkUrl(req: HttpRequest<any>) {
+    const url = req.url.toLowerCase();
+    const found = this.silentApis.find((u) => url.startsWith(u));
+    return !!found;
   }
 
 }
