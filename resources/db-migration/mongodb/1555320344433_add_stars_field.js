2ed0cf97074ca183f6d1f147e62ffbd6910899a7
Adrian Matei <ama@codepedia.org>
2019-04-15 16:54:31 +0300

    reformat mongo script to look better

--- resources/db-migration/mongodb/1555320344433_add_stars_field.js
+++ resources/db-migration/mongodb/1555320344433_add_stars_field.js
@@ -1,6 +1,13 @@
 db.bookmarks.find().forEach(function(results)
 {
   print( "Id: " + results._id );
-  db.bookmarks.update( {_id : results._id},
-    { $set: {"stars": results.starredBy ? results.starredBy.length : 0} })
+  db.bookmarks.update(
+    {
+      _id : results._id
+    },
+    {
+      $set: {
+        "stars": results.starredBy ? results.starredBy.length : 0
+      }
+    })
 });
