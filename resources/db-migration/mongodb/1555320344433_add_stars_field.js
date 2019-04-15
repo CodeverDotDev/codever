f0fa8442f37f1edc873e4cf21f27444fe995144b
Adrian Matei <ama@codingpedia.org>
2019-04-15 12:27:37 +0300

    add stars field to bookmarks collection

---
+++ resources/db-migration/mongodb/1555320344433_add_stars_field.js
@@ -0,0 +1,6 @@
+db.bookmarks.find().forEach(function(results)
+{
+  print( "Id: " + results._id );
+  db.bookmarks.update( {_id : results._id},
+    { $set: {"stars": results.starredBy ? results.starredBy.length : 0} })
+});
