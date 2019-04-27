f942b95db4078d1595b060fb030860814a93e781
Adrian Matei <ama@codingpedia.org>
2019-04-27 14:51:37 +0200

    move public search function to own search to create the base for common functionality (public and personal)

--- routes/public-bookmarks.js
+++ routes/public-bookmarks.js
@@ -5,7 +5,8 @@ var router = express.Router();
 var Bookmark = require('../models/bookmark');
 var HttpStatus = require('http-status-codes');
 var MyError = require('../models/error');
-const escapeStringRegexp = require('escape-string-regexp');
+
+const bookmarksSearchService = require('../common/bookmarks-search.service');
 
 const MAX_NUMBER_RETURNED_RESULTS = 100;
 
@@ -17,17 +18,17 @@ router.get('/', async (req, res) => {
     if ( req.query.query ) {
       //split in text and tags
       const limit = parseInt(req.query.limit);
-      const searchedTermsAndTags = splitSearchQuery(req.query.query);
+      const searchedTermsAndTags = bookmarksSearchService.splitSearchQuery(req.query.query);
       const searchedTerms = searchedTermsAndTags[0];
       const searchedTags = searchedTermsAndTags[1];
       let bookmarks = [];
       const lang = req.query.lang;
       if ( searchedTerms.length > 0 && searchedTags.length > 0 ) {
-        bookmarks = await getBookmarksForTagsAndTerms(bookmarks, searchedTags, searchedTerms, limit);
+        bookmarks = await bookmarksSearchService.getBookmarksForTagsAndTerms(bookmarks, searchedTags, searchedTerms, limit);
       } else if ( searchedTerms.length > 0 ) {
-        bookmarks = await getBookmarksForSearchedTerms(searchedTerms, bookmarks, limit);
+        bookmarks = await bookmarksSearchService.getBookmarksForSearchedTerms(searchedTerms, bookmarks, limit);
       } else {
-        bookmarks = await getBookmarksforSearchedTags(bookmarks, searchedTags, limit);
+        bookmarks = await bookmarksSearchService.getBookmarksForSearchedTags(bookmarks, searchedTags, limit);
       }
       if ( lang && lang !== 'all' ) {
         bookmarks = bookmarks.filter(x => x.language === lang);
@@ -54,133 +55,6 @@ router.get('/', async (req, res) => {
     return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err);
   }
 });
-
-let getBookmarksForTagsAndTerms = async function (bookmarks, searchedTags, searchedTerms, limit) {
-  bookmarks = await Bookmark.find(
-    {
-      shared: true,
-      tags:
-        {
-          $all: searchedTags
-        },
-      $text:
-        {
-          $search: searchedTerms.join(' ')
-        }
-    },
-    {
-      score: {$meta: "textScore"}
-    }
-  )
-  //.sort({createdAt: -1})
-    .sort({score: {$meta: "textScore"}})
-    .lean()
-    .exec();
-
-  for ( const term of searchedTerms ) {
-    bookmarks = bookmarks.filter(x => bookmarkContainsSearchedTerm(x, term.trim()));
-  }
-  bookmarks = bookmarks.slice(0, limit);
-
-  return bookmarks;
-};
-
-let getBookmarksForSearchedTerms = async function (searchedTerms, bookmarks, limit) {
-  const termsJoined = searchedTerms.join(' ');
-  const termsQuery = escapeStringRegexp(termsJoined);
-  bookmarks = await Bookmark.find(
-    {
-      shared: true,
-      $text: {$search: termsQuery},
-    },
-    {
-      score: {$meta: "textScore"}
-    }
-  )
-  //.sort({createdAt: -1}) //let's give it a try with text score
-    .sort({score: {$meta: "textScore"}})
-    .lean()
-    .exec();
-
-  for ( const term of searchedTerms ) {
-    bookmarks = bookmarks.filter(x => bookmarkContainsSearchedTerm(x, term.trim()));
-  }
-  bookmarks = bookmarks.slice(0, limit);
-
-  return bookmarks;
-};
-
-let getBookmarksforSearchedTags = async function (bookmarks, searchedTags, limit) {
-  console.log('searchedTags', searchedTags);
-  bookmarks = await Bookmark.find(
-    {
-      shared: true,
-      tags:
-        {
-          $all: searchedTags
-        },
-    }
-  )
-    .sort({createdAt: -1})
-    .limit(limit)
-    .lean()
-    .exec();
-  return bookmarks;
-};
-
-function bookmarkContainsSearchedTerm(bookmark, searchedTerm) {
-  let result = false;
-  // const escapedSearchPattern = '\\b' + this.escapeRegExp(searchedTerm.toLowerCase()) + '\\b'; word boundary was not enough, especially for special characters which can happen in coding
-  // https://stackoverflow.com/questions/23458872/javascript-regex-word-boundary-b-issue
-  const separatingChars = '\\s\\.,;#\\-\\/_\\[\\]\\(\\)\\*\\+';
-  const escapedSearchPattern = `(^|[${separatingChars}])(${escapeRegExp(searchedTerm.toLowerCase())})(?=$|[${separatingChars}])`;
-  const pattern = new RegExp(escapedSearchPattern);
-  if ( (bookmark.name && pattern.test(bookmark.name.toLowerCase()))
-    || (bookmark.location && pattern.test(bookmark.location.toLowerCase()))
-    || (bookmark.description && pattern.test(bookmark.description.toLowerCase()))
-    || (bookmark.githubURL && pattern.test(bookmark.githubURL.toLowerCase()))
-  ) {
-    result = true;
-  }
-
-  if ( result ) {
-    return true;
-  } else {
-    // if not found already look through the tags also
-    bookmark.tags.forEach(tag => {
-      if ( pattern.test(tag.toLowerCase()) ) {
-        result = true;
-      }
-    });
-  }
-
-  return result;
-}
-
-function escapeRegExp(str) {
-  const specials = [
-      // order matters for these
-      '-'
-      , '['
-      , ']'
-      // order doesn't matter for any of these
-      , '/'
-      , '{'
-      , '}'
-      , '('
-      , ')'
-      , '*'
-      , '+'
-      , '?'
-      , '.'
-      , '\\'
-      , '^'
-      , '$'
-      , '|'
-    ],
-    regex = RegExp('[' + specials.join('\\') + ']', 'g');
-  return str.replace(regex, '\\$&'); // $& means the whole matched string
-}
 
 router.get('/tagged/:tag', async (req, res) => {
   try {
@@ -270,68 +144,5 @@ router.get('/advanced-search', function (req, res, next) {
 
 });
 
-function splitSearchQuery(query) {
-
-  const result = [[], []];
-
-  const terms = [];
-  let term = '';
-  const tags = [];
-  let tag = '';
-
-  let isInsideTerm = false;
-  let isInsideTag = false;
-
-
-  for ( let i = 0; i < query.length; i++ ) {
-    const currentCharacter = query[i];
-    if ( currentCharacter === ' ' ) {
-      if ( !isInsideTag ) {
-        if ( !isInsideTerm ) {
-          continue;
-        } else {
-          terms.push(term);
-          isInsideTerm = false;
-          term = '';
-        }
-      } else {
-        tag += ' ';
-      }
-    } else if ( currentCharacter === '[' ) {
-      if ( isInsideTag ) {
-        tags.push(tag.trim());
-        tag = '';
-      } else {
-        isInsideTag = true;
-      }
-    } else if ( currentCharacter === ']' ) {
-      if ( isInsideTag ) {
-        isInsideTag = false;
-        tags.push(tag.trim());
-        tag = '';
-      }
-    } else {
-      if ( isInsideTag ) {
-        tag += currentCharacter;
-      } else {
-        isInsideTerm = true;
-        term += currentCharacter;
-      }
-    }
-  }
-
-  if ( tag.length > 0 ) {
-    tags.push(tag.trim());
-  }
-
-  if ( term.length > 0 ) {
-    terms.push(term);
-  }
-
-  result[0] = terms;
-  result[1] = tags;
-
-  return result;
-}
 
 module.exports = router;
