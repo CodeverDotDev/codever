8241dfff5058154f6bf585fe9742c646db9893c4
Adrian Matei <ama@codepedia.org>
2019-05-06 21:05:17 +0200

    fix integration tests

--- routes/public-bookmarks.spec.js
+++ routes/public-bookmarks.spec.js
@@ -130,7 +130,7 @@ describe('Public API Tests', function () {
     it('should find bookmark with with very-special-tag in query param as word', function (done) {
       request(app)
         .get(baseApiUnderTestUrl)
-        .query({query: "very-special-tag"})
+        .query({q: "very-special-tag"})
         .query({limit: 10})
         .end(function (err, response) {
           expect(response.statusCode).to.equal(HttpStatus.OK);
@@ -145,7 +145,7 @@ describe('Public API Tests', function () {
     it('should find bookmark with with very-special-tag in query param only as tag', function (done) {
       request(app)
         .get(baseApiUnderTestUrl)
-        .query({query: `[${verySpecialTag}]`})
+        .query({q: `[${verySpecialTag}]`})
         .query({limit: 10})
         .end(function (err, response) {
           expect(response.statusCode).to.equal(HttpStatus.OK);
@@ -160,7 +160,7 @@ describe('Public API Tests', function () {
     it('should find bookmark with with very-special-tag in query param as tag and word', function (done) {
       request(app)
         .get(baseApiUnderTestUrl)
-        .query({query: `${verySpecialTag} [${verySpecialTag}]`})
+        .query({q: `${verySpecialTag} [${verySpecialTag}]`})
         .query({limit: 10})
         .end(function (err, response) {
           expect(response.statusCode).to.equal(HttpStatus.OK);
@@ -175,7 +175,7 @@ describe('Public API Tests', function () {
     it(`should find bookmark with special title - ${verySpecialTitle} `, function (done) {
       request(app)
         .get(baseApiUnderTestUrl)
-        .query({query: `${verySpecialTitle}`})
+        .query({q: `${verySpecialTitle}`})
         .query({limit: 10})
         .end(function (err, response) {
           expect(response.statusCode).to.equal(HttpStatus.OK);
@@ -190,7 +190,7 @@ describe('Public API Tests', function () {
     it(`should find bookmark with special source code url title - ${verySpecialSourceCodeUrl} `, function (done) {
       request(app)
         .get(baseApiUnderTestUrl)
-        .query({query: `${verySpecialSourceCodeUrl}`})
+        .query({q: `${verySpecialSourceCodeUrl}`})
         .query({limit: 10})
         .end(function (err, response) {
           expect(response.statusCode).to.equal(HttpStatus.OK);
@@ -205,7 +205,7 @@ describe('Public API Tests', function () {
     it(`should find bookmark with special location - ${verySpecialLocation} `, function (done) {
       request(app)
         .get(baseApiUnderTestUrl)
-        .query({query: `${verySpecialLocation}`})
+        .query({q: `${verySpecialLocation}`})
         .query({limit: 10})
         .end(function (err, response) {
           expect(response.statusCode).to.equal(HttpStatus.OK);
