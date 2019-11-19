77cd181e36e4c6f1f78128bcc446ee0c4f5d7d04
Adrian Matei <ama@codepedia.org>
2019-11-19 08:00:33 +0100

    unify error-handling, unify userId validation and create services for routers

---
+++ src/app.js
@@ -0,0 +1,185 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const apiBasePathRouter = require('./routes');
+const versionRouter = require('./routes/version/version');
+const userRouter = require('./routes/users/user.router');
+const adminRouter = require('./routes/admin/admin.router');
+const publicBookmarksRouter = require('./routes/public/public-bookmarks.router');
+const {MongoError} = require('mongodb');
+const ValidationError = require('./error/validation.error');
+const NotFoundError = require('./error/not-found.error');
+const PublicBookmarkExistingError = require('./error/public-bookmark-existent.error');
+const UseridTokenValidationError = require('./routes/users/userid-validation.error');
+
+const fs = require('fs-extra');
+const rfs = require('rotating-file-stream/index');
+
+const HttpStatus = require('http-status-codes/index');
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./docs/swagger.yaml');
+
+const app = express();
+
+const mongoUserName = process.env.MONGODB_BOOKMARKS_USERNAME || 'bookmarks';
+const mongoUserPwd = process.env.MONGODB_BOOKMARKS_PASSWORD || 'secret';
+const mongoBookmarksCollectionName = process.env.MONGODB_BOOKMARKS_COLLECTION || 'dev-bookmarks';
+const mongoHost = process.env.MONGODB_HOST || 'localhost';
+const mongoPort = process.env.MONGODB_PORT || '27017';
+
+const mongooseConnectOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+};
+
+const mongoUrl = `mongodb://${mongoUserName}:${mongoUserPwd}@${mongoHost}:${mongoPort}/${mongoBookmarksCollectionName}`;
+mongoose.connect(mongoUrl, mongooseConnectOptions);
+
+// sets port 3000 to default or unless otherwise specified in the environment
+app.set('port', process.env.PORT || 3000);
+
+let setUpLogging = function () {
+  const logDirectory = (process.env.CONTAINER_HOME || '.') + '/log';
+  // ensure log directory exists
+  fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+  // create a rotating write stream
+  let accessLogStream = rfs('access.log', {
+    interval: '1d', // rotate daily
+    path: logDirectory
+  })
+  app.use(logger('combined', {stream: accessLogStream}));// logs in file in Apache style format
+  app.use(logger('dev'));// logs at the console in 'dev' format
+};
+
+setUpLogging();
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));//swagger docs are not protected
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//add CORS support
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Location');
+  res.setHeader('Access-Control-Expose-Headers', 'Content-Type, Authorization, Location');
+  next();
+});
+
+
+app.use('/api', apiBasePathRouter);
+app.use('/api/version', versionRouter);
+app.use('/api/public/bookmarks', publicBookmarksRouter);
+app.use('/api/personal/users', userRouter);
+app.use('/api/admin', adminRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = HttpStatus.NOT_FOUND;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res) {
+    res.status(err.status || HttpStatus.INTERNAL_SERVER_ERROR);
+    res.render({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.use(function handleNotFoundError(error, req, res, next) {
+  if (error instanceof NotFoundError) {
+    return res.status(HttpStatus.NOT_FOUND).send({
+      message: error.message,
+      error: {}
+    });
+  }
+  next(error);
+});
+
+app.use(function handlePublicBookmarkExistingError(error, req, res, next) {
+  if (error instanceof PublicBookmarkExistingError) {
+    return res.status(HttpStatus.CONFLICT).send({
+      message: error.message,
+      error: {}
+    });
+  }
+  next(error);
+});
+
+app.use(function handleUserIdValidationError(error, req, res, next) {
+  if (error instanceof UseridTokenValidationError) {
+    res.status(HttpStatus.UNAUTHORIZED);
+    return res.send({
+      httpStatus: HttpStatus.UNAUTHORIZED,
+      message: error.message
+    });
+  }
+  next(error);
+});
+
+app.use(function handleValidationError(error, request, response, next) {
+  if (error instanceof ValidationError) {
+    return response
+      .status(HttpStatus.BAD_REQUEST)
+      .json({
+        httpStatus: HttpStatus.BAD_REQUEST,
+        message: error.message,
+        validationErrors: error.validationErrors
+      });
+  }
+  next(error);
+});
+
+app.use(function handleDatabaseError(error, request, response, next) {
+  if (error instanceof MongoError) {
+    if (error.code === 11000) {
+      return response
+        .status(HttpStatus.CONFLICT)
+        .json({
+          type: 'MongoError',
+          message: error.message
+        });
+    } else {
+      return response.status(503).json({
+        type: 'MongoError',
+        message: error.message
+      });
+    }
+  }
+  next(error);
+});
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (error, req, res, next) {
+  if (res.headersSent) {
+    return next(error)
+  } else {
+    res.status(error.status || HttpStatus.INTERNAL_SERVER_ERROR);
+    res.send({
+      message: error.message,
+      error: {}
+    });
+  }
+
+});
+
+module.exports = app;
---
+++ src/routes/public/public-bookmarks.router.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var router = express.Router();
+const constants = require('../../common/constants');
+
+const AsyncWrapper = require('../../common/async-wrapper');
+const ValidationError = require('../../error/validation.error');
+
+const bookmarksSearchService = require('../../common/bookmarks-search.service');
+const PublicBookmarksService = require('./public-bookmarks.service');
+
+/**
+ *  Returns the with query text
+ */
+router.get('/', AsyncWrapper.wrapAsync(async (request, response, next) => {
+  const searchText = request.query.q;
+  const limit = parseInt(request.query.limit);
+  if (searchText) {
+    const bookmarks = await bookmarksSearchService.findBookmarks(searchText, limit, constants.DOMAIN_PUBLIC, null);
+    response.send(bookmarks);
+  } else {
+    next()
+  }
+}));
+
+/**
+ * Get Bookmark by location
+ */
+router.get('/', AsyncWrapper.wrapAsync(async (request, response, next) => {
+  const location = request.query.location;
+  if (location) {
+    const bookmark = await PublicBookmarksService.getBookmarkByLocation(location);
+
+    return response.send(bookmark);
+  } else {
+    next()
+  }
+}));
+
+/**
+ * When no filter send latest public bookmarks
+ */
+router.get('/', AsyncWrapper.wrapAsync(async (request, response) => {
+  const bookmarks = await PublicBookmarksService.getLatestBookmarks();
+
+  return response.send(bookmarks);
+}));
+
+router.get('/tagged/:tag', AsyncWrapper.wrapAsync(async (request, response) => {
+  const orderByFilter = request.query.orderBy === 'STARS' ? {likes: -1} : {createdAt: -1};
+  const bookmarks = await PublicBookmarksService.getBookmarksForTag(request.params.tag, orderByFilter);
+
+  return response.send(bookmarks);
+}));
+
+/* GET title of bookmark given its url - might be moved to front-end */
+router.get('/scrape', async function (request, response, next) {
+  const location = request.query.location;
+  if (location) {
+    const webpageData = await PublicBookmarksService.getScrapedDataForLocation(location);
+
+    return response.send(webpageData);
+  } else {
+    next();
+  }
+});
+
+/* GET youtube video data */
+router.get('/scrape', AsyncWrapper.wrapAsync(async function (request, response, next) {
+  const youtubeVideoId = request.query.youtubeVideoId;
+  if (youtubeVideoId) {
+    const webpageData = await PublicBookmarksService.getYoutubeVideoData(youtubeVideoId)
+
+    return response.send(webpageData);
+  } else {
+    next();
+  }
+}));
+
+/* GET title of bookmark given its url */
+router.get('/scrape', AsyncWrapper.wrapAsync(async function () {
+  throw new ValidationError('Missing parameters - url or youtubeVideoId',
+    ['You need to provide the url to scrape for or the youtubeVideoId']);
+}));
+
+/**
+ *  GET bookmark by id.
+ *  This needs to be the last call to avoid to "tagged" and "scrape" endpoints, which throw then errors
+ */
+
+router.get('/:id', AsyncWrapper.wrapAsync(async function (request, response) {
+  const bookmark = await PublicBookmarksService.getBookmarkById(request.params.id);
+  response.send(bookmark);
+}));
+
+
+module.exports = router;
