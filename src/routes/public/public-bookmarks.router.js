8ac30faf2156c0586a38d3a47f758f5fc2dfe8a6
Adrian Matei <ama@codepedia.org>
2019-12-04 13:59:21 +0100

    replace asyncwrapper with express-async-errors module

--- src/routes/public/public-bookmarks.router.js
+++ src/routes/public/public-bookmarks.router.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const constants = require('../../common/constants');
 
-const AsyncWrapper = require('../../common/async-wrapper');
 const ValidationError = require('../../error/validation.error');
 
 const bookmarksSearchService = require('../../common/bookmarks-search.service');
@@ -11,7 +10,7 @@ const PublicBookmarksService = require('./public-bookmarks.service');
 /**
  *  Returns the with query text
  */
-router.get('/', AsyncWrapper.wrapAsync(async (request, response, next) => {
+router.get('/', async (request, response, next) => {
   const searchText = request.query.q;
   const limit = parseInt(request.query.limit);
   if (searchText) {
@@ -20,12 +19,12 @@ router.get('/', AsyncWrapper.wrapAsync(async (request, response, next) => {
   } else {
     next()
   }
-}));
+});
 
 /**
  * Get Bookmark by location
  */
-router.get('/', AsyncWrapper.wrapAsync(async (request, response, next) => {
+router.get('/', async (request, response, next) => {
   const location = request.query.location;
   if (location) {
     const bookmark = await PublicBookmarksService.getBookmarkByLocation(location);
@@ -34,23 +33,23 @@ router.get('/', AsyncWrapper.wrapAsync(async (request, response, next) => {
   } else {
     next()
   }
-}));
+});
 
 /**
  * When no filter send latest public bookmarks
  */
-router.get('/', AsyncWrapper.wrapAsync(async (request, response) => {
+router.get('/', async (request, response) => {
   const bookmarks = await PublicBookmarksService.getLatestBookmarks();
 
   return response.send(bookmarks);
-}));
+});
 
-router.get('/tagged/:tag', AsyncWrapper.wrapAsync(async (request, response) => {
+router.get('/tagged/:tag', async (request, response) => {
   const orderByFilter = request.query.orderBy === 'STARS' ? {likes: -1} : {createdAt: -1};
   const bookmarks = await PublicBookmarksService.getBookmarksForTag(request.params.tag, orderByFilter);
 
   return response.send(bookmarks);
-}));
+});
 
 /* GET title of bookmark given its url - might be moved to front-end */
 router.get('/scrape', async function (request, response, next) {
@@ -65,7 +64,7 @@ router.get('/scrape', async function (request, response, next) {
 });
 
 /* GET youtube video data */
-router.get('/scrape', AsyncWrapper.wrapAsync(async function (request, response, next) {
+router.get('/scrape', async function (request, response, next) {
   const youtubeVideoId = request.query.youtubeVideoId;
   if (youtubeVideoId) {
     const webpageData = await PublicBookmarksService.getYoutubeVideoData(youtubeVideoId)
@@ -74,23 +73,23 @@ router.get('/scrape', AsyncWrapper.wrapAsync(async function (request, response, 
   } else {
     next();
   }
-}));
+});
 
 /* GET title of bookmark given its url */
-router.get('/scrape', AsyncWrapper.wrapAsync(async function () {
+router.get('/scrape', async function () {
   throw new ValidationError('Missing parameters - url or youtubeVideoId',
     ['You need to provide the url to scrape for or the youtubeVideoId']);
-}));
+});
 
 /**
  *  GET bookmark by id.
  *  This needs to be the last call to avoid to "tagged" and "scrape" endpoints, which throw then errors
  */
 
-router.get('/:id', AsyncWrapper.wrapAsync(async function (request, response) {
+router.get('/:id', async function (request, response) {
   const bookmark = await PublicBookmarksService.getBookmarkById(request.params.id);
   response.send(bookmark);
-}));
+});
 
 
 module.exports = router;
--- src/routes/users/bookmarks/personal-bookmarks.router.js
+++ src/routes/users/bookmarks/personal-bookmarks.router.js
@@ -6,7 +6,6 @@ const bookmarkHelper = require('../../../common/bookmark-helper');
 const bookmarksSearchService = require('../../../common/bookmarks-search.service');
 const PersonalBookmarksService = require('./personal-bookmarks.service');
 const UserIdValidator = require('../userid.validator');
-const AsyncWrapper = require('../../../common/async-wrapper');
 
 const ValidationError = require('../../../error/validation.error');
 
@@ -25,7 +24,8 @@ personalBookmarksRouter.use(keycloak.middleware());
 /**
  * CREATE bookmark for user
  */
-personalBookmarksRouter.post('/', keycloak.protect(), AsyncWrapper.wrapAsync(async (request, response) => {
+//personalBookmarksRouter.post('/', keycloak.protect(), async (request, response) => {
+personalBookmarksRouter.post('/', keycloak.protect(), async (request, response) => {
 
   UserIdValidator.validateUserId(request);
   const bookmark = bookmarkHelper.buildBookmarkFromRequest(request);
@@ -36,10 +36,10 @@ personalBookmarksRouter.post('/', keycloak.protect(), AsyncWrapper.wrapAsync(asy
     .status(HttpStatus.CREATED)
     .send({response: 'Bookmark created for userId ' + request.params.userId});
 
-}));
+});
 
 /* GET bookmark of user  by id*/
-personalBookmarksRouter.get('/', keycloak.protect(), AsyncWrapper.wrapAsync(async (request, response, next) => {
+personalBookmarksRouter.get('/', keycloak.protect(), async (request, response, next) => {
   UserIdValidator.validateUserId(request);
 
   const searchText = request.query.q;
@@ -52,48 +52,48 @@ personalBookmarksRouter.get('/', keycloak.protect(), AsyncWrapper.wrapAsync(asyn
     next();
   }
 
-}));
+});
 
 /* GET bookmark of user by location*/
-personalBookmarksRouter.get('/', keycloak.protect(), AsyncWrapper.wrapAsync(async (request, response, next) => {
+personalBookmarksRouter.get('/', keycloak.protect(), async (request, response, next) => {
   if (request.query.location) {
     const bookmark = await PersonalBookmarksService.getBookmarkByLocation(request.params.userId, request.query.location);
     return response.send(bookmark);
   } else {
     next();
   }
-}));
+});
 
 /* GET bookmark of user */
-personalBookmarksRouter.get('/', keycloak.protect(), AsyncWrapper.wrapAsync(async (request, response) => {
+personalBookmarksRouter.get('/', keycloak.protect(), async (request, response) => {
   const bookmarks = await PersonalBookmarksService.getLatestBookmarks(request.params.userId);
   return response.send(bookmarks);
-}));
+});
 
 /* GET tags used by user */
-personalBookmarksRouter.get('/tags', keycloak.protect(), AsyncWrapper.wrapAsync(async (request, response) => {
+personalBookmarksRouter.get('/tags', keycloak.protect(), async (request, response) => {
   UserIdValidator.validateUserId(request);
   const tags = await PersonalBookmarksService.getTagsForUser(request.params.userId);
 
   response.send(tags);
-}));
+});
 
 
 /* GET bookmark of user */
-personalBookmarksRouter.get('/:bookmarkId', keycloak.protect(), AsyncWrapper.wrapAsync(async (request, response) => {
+personalBookmarksRouter.get('/:bookmarkId', keycloak.protect(), async (request, response) => {
   UserIdValidator.validateUserId(request);
 
   const {userId, bookmarkId} = request.params;
   const bookmark = await PersonalBookmarksService.getBookmarkById(userId, bookmarkId);
 
   return response.status(HttpStatus.OK).send(bookmark);
-}));
+});
 
 /**
  * full UPDATE via PUT - that is the whole document is required and will be updated
  * the descriptionHtml parameter is only set in backend, if only does not come front-end (might be an API call)
  */
-personalBookmarksRouter.put('/:bookmarkId', keycloak.protect(), AsyncWrapper.wrapAsync(async (request, response) => {
+personalBookmarksRouter.put('/:bookmarkId', keycloak.protect(), async (request, response) => {
 
   UserIdValidator.validateIsAdminOrUserId(request);
 
@@ -103,23 +103,23 @@ personalBookmarksRouter.put('/:bookmarkId', keycloak.protect(), AsyncWrapper.wra
   const updatedBookmark = await PersonalBookmarksService.updateBookmark(userId, bookmarkId, bookmark);
 
   return response.status(HttpStatus.OK).send(updatedBookmark);
-}));
+});
 
 /*
 * DELETE bookmark for user
 */
-personalBookmarksRouter.delete('/:bookmarkId', keycloak.protect(), AsyncWrapper.wrapAsync(async (request, response) => {
+personalBookmarksRouter.delete('/:bookmarkId', keycloak.protect(), async (request, response) => {
 
   UserIdValidator.validateIsAdminOrUserId(request);
 
   await PersonalBookmarksService.deleteBookmarkById(request.params.userId, request.params.bookmarkId);
   return response.status(HttpStatus.NO_CONTENT).send();
-}));
+});
 
 /*
 * DELETE bookmark for user by location
 */
-personalBookmarksRouter.delete('/', keycloak.protect(), AsyncWrapper.wrapAsync(async (request, response, next) => {
+personalBookmarksRouter.delete('/', keycloak.protect(), async (request, response, next) => {
   UserIdValidator.validateIsAdminOrUserId(request);
 
   const location = request.query.location;
@@ -130,11 +130,11 @@ personalBookmarksRouter.delete('/', keycloak.protect(), AsyncWrapper.wrapAsync(a
   }
 
   return response.status(HttpStatus.NO_CONTENT).send();
-}));
+});
 
-personalBookmarksRouter.delete('/', keycloak.protect(), AsyncWrapper.wrapAsync(async () => {
+personalBookmarksRouter.delete('/', keycloak.protect(), async () => {
   throw new ValidationError('Missing parameters',
     ['You need to provide bookmark id or location to delete personal bookmarks']);
-}));
+});
 
 module.exports = personalBookmarksRouter;
