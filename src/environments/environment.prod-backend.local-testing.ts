bb4c8732be596732d96886cb159c89669b71fd59
Adrian Matei <ama@codepedia.org>
2019-12-15 09:56:01 +0100

    add create bookmark via bookmarklet functionality
    A link with the bookmarklet is placed in the navigation menu. You just
    need to drag it to your bookmarks toolbar for rapid access

--- src/environments/environment.prod-backend.local-testing.ts
+++ src/environments/environment.prod-backend.local-testing.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     APP_HOME_URL: 'http://localhost:4200',
     API_URL: 'https://www.bookmarks.dev/api',
-    HOST: 'https://www.bookmarks.dev/',
+    HOST: 'https://www.bookmarks.dev',
     keycloak:  {
         'realm': 'bookmarks',
         'url': 'https://www.bookmarks.dev/auth',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   APP_HOME_URL: 'http://localhost:4200',
   API_URL: 'http://localhost:3000/api',
-  HOST: 'http://localhost:4200/',
+  HOST: 'http://localhost:4200',
   keycloak:  {
     'realm': 'bookmarks',
     'url': 'http://localhost:8480/auth',
