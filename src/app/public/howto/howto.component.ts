34ae0f60da7d733708d9dafa379fdc09066c94fb
Adrian Matei <ama@codingpedia.org>
2020-01-02 07:06:35 +0100

    improve content and linking in HowTo and About pages

--- src/app/public/howto/howto.component.ts
+++ src/app/public/howto/howto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-howto',
@@ -7,12 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HowtoComponent implements OnInit {
 
-  constructor() {
-    // Do stuff
-  }
+  environment = environment;
 
-  ngOnInit() {
-    console.log('Hello howto');
-  }
+  ngOnInit() {}
 
 }
