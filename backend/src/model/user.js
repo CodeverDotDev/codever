8f8e3b9cae9ccdaf00327eb5535132e188d4cd0b
Adrian Matei <ama@codepedia.org>
2020-05-29 20:06:48 +0200

    fix(saved-searches-page): add saved searches for my-codelets
    and the possibility to delete only for the selected search-domain

--- backend/src/model/user.js
+++ backend/src/model/user.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const searchSchema = new Schema({
-    _id: {type:Schema.Types.ObjectId, select: false},
     text: String,
     lastAccessedAt: Date,
     searchDomain: String,
