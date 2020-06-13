0e2ad1e3cb4aae28c524b038b5ff52d56e387b36
Adrian Matei <ama@codepedia.org>
2020-06-13 06:46:32 +0200

    feat(fe-hot-keys): add hot keys dialog (ctrl+h for history) and (ctrl+p for pinned)

---
+++ frontend/src/app/public/howto/howto-hotkeys/howto-hotkeys.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HowtoHotkeysComponent } from './howto-hotkeys.component';
+
+describe('HowtoHotkeysComponent', () => {
+  let component: HowtoHotkeysComponent;
+  let fixture: ComponentFixture<HowtoHotkeysComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HowtoHotkeysComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HowtoHotkeysComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/public/howto/howto-hotkeys/howto-hotkeys.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-howto-hotkeys',
+  templateUrl: './howto-hotkeys.component.html',
+  styleUrls: ['./howto-hotkeys.component.scss']
+})
+export class HowtoHotkeysComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
