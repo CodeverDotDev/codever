1b8ba3586ae8375b8aff843e7df13a0e52350a20
Adrian Matei <ama@codepedia.org>
2020-06-14 20:06:16 +0200

    feat(fe-bookmark-details): works initially except feed pagination...

---
+++ frontend/src/app/shared/bookmark-list-element/bookmark-list-element.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookmarkListElementComponent } from './bookmark-list-element.component';
+
+describe('BookmarkListElementComponent', () => {
+  let component: BookmarkListElementComponent;
+  let fixture: ComponentFixture<BookmarkListElementComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BookmarkListElementComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookmarkListElementComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
