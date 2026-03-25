e62ef4771d173c0c3782e8d2c66eb7535609c6ba
adirochs <ama@codepedia.org>
2026-03-25 17:30:14 +0100

    feat(notes): highlight code in notes

--- apps/codever-ui/src/app/shared/pipe/markdown2html.pipe.ts
+++ apps/codever-ui/src/app/shared/pipe/markdown2html.pipe.ts
@@ -2,6 +2,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 import * as DOMPurify from 'dompurify';
 
 import { marked } from 'marked';
+import hljs from 'highlight.js';
+
+marked.setOptions({
+  highlight: function (code: string, lang: string) {
+    if (lang && hljs.getLanguage(lang)) {
+      return hljs.highlight(code, { language: lang }).value;
+    }
+    return hljs.highlightAuto(code).value;
+  },
+});
 
 @Pipe({ name: 'md2html' })
 export class Markdown2HtmlPipe implements PipeTransform {
